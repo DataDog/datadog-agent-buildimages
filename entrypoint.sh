4618467e1ef290194ccdb85f01531abc8da7e218
Albert Vaca <albert.vaca@datadoghq.com>
2020-02-10 18:45:45 +0100

    Fix conda activate on ARM
    We use the system python instead of conda.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -l
 set -e
 
-source /root/.bashrc && conda activate ddpy3
+source /root/.bashrc
+
+conda activate ddpy3 || true # We use system python3 on some images, allow this to fail
+
 eval "$(gimme)"
 
 exec "$@"
