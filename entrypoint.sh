49bb254768288f2738cd8d85529a2e899df161e3
Julien Lebot <julien.lebot@datadoghq.com>
2020-06-10 15:05:19 +0200

    Add GIMME_ARCH in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,11 @@ source /root/.bashrc
 
 conda activate ddpy3 || true # We use system python3 on some images, allow this to fail
 
+if [ "$TARGET_ARCH" = "arm64v8" ] ; then
+    export GIMME_ARCH=arm64
+else
+    export GIMME_ARCH=arm
+fi
 eval "$(gimme)"
 
 exec "$@"
