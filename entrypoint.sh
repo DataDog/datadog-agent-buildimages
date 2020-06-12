3b294e20c34692d9613562a8371dbf2c9feb0bed
Julien Lebot <julien.lebot@datadoghq.com>
2020-06-12 17:42:32 +0200

    Replace TARGET_ARCH -> DD_TARGET_ARCH because it can conflict with some software Add missing dependency for RPM

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,9 @@ source /root/.bashrc
 
 conda activate ddpy3 || true # We use system python3 on some images, allow this to fail
 
-if [ "$TARGET_ARCH" = "arm64v8" ] ; then
+if [ "$DD_TARGET_ARCH" = "arm64v8" ] ; then
     export GIMME_ARCH=arm64
-elif [ "$TARGET_ARCH" = "arm32v7" ] ; then
+elif [ "$DD_TARGET_ARCH" = "arm32v7" ] ; then
     export GIMME_ARCH=arm
 fi
 eval "$(gimme)"
