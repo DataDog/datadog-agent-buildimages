1129f60e86ee6fdb1d62f3805dfc1cf6b4b7e761
Julien Lebot <julien.lebot@datadoghq.com>
2020-06-17 11:43:40 +0200

    Fixes for ARM 32/64 (#49)
    Fix 64 bit ARM builders

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,11 @@ source /root/.bashrc
 
 conda activate ddpy3 || true # We use system python3 on some images, allow this to fail
 
+if [ "$DD_TARGET_ARCH" = "arm64v8" ] ; then
+    export GIMME_ARCH=arm64
+elif [ "$DD_TARGET_ARCH" = "arm32v7" ] ; then
+    export GIMME_ARCH=arm
+fi
 eval "$(gimme)"
 
 exec "$@"
