2fce3064426cccd5b8326047a5013871aa4307da
Kylian Serrania <kylian.serrania@datadoghq.com>
2020-08-27 08:50:32 +0200

    [macos] Allow customizing the keychain name

--- macos/certificate_setup.sh
+++ macos/certificate_setup.sh
@@ -10,17 +10,20 @@
 # - developer_id_application.p12 signing certificate available in $HOME folder
 # - $INSTALLER_CERTIFICATE_PWD contains the password of the developer_id_installer.p12 file
 # - $APPLICATION_CERTIFICATE_PWD contains the password of the developer_id_application.p12 file
-# - $KEYCHAIN_PWD contains the login keychain password
+# - $KEYCHAIN_NAME contains the keychain name. Defaults to login.keychain
+# - $KEYCHAIN_PWD contains the keychain password
+
+export KEYCHAIN_NAME=${KEYCHAIN_NAME:-"login.keychain"}
 
 # Unlock login keychain
-security unlock-keychain -p $KEYCHAIN_PWD "login.keychain"
+security unlock-keychain -p "$KEYCHAIN_PWD" "$KEYCHAIN_NAME"
 
 # Import signing certificates and allow relevant apps to use them
-security import ~/developer_id_installer.p12 -P $INSTALLER_CERTIFICATE_PWD -k "login.keychain" -T /usr/bin/productbuild
-security import ~/developer_id_application.p12 -P $APPLICATION_CERTIFICATE_PWD -k "login.keychain" -T /usr/bin/codesign
+security import ~/developer_id_installer.p12 -P "$INSTALLER_CERTIFICATE_PWD" -k "$KEYCHAIN_NAME" -T /usr/bin/productbuild
+security import ~/developer_id_application.p12 -P "$APPLICATION_CERTIFICATE_PWD" -k "$KEYCHAIN_NAME" -T /usr/bin/codesign
 
 # Update key partition list
-security set-key-partition-list -S apple-tool:,apple: -s -k $KEYCHAIN_PWD login.keychain
+security set-key-partition-list -S apple-tool:,apple: -s -k "$KEYCHAIN_PWD" "$KEYCHAIN_NAME"
 
 # Lock login keychain
-security lock-keychain "login.keychain"
+security lock-keychain "$KEYCHAIN_NAME"
