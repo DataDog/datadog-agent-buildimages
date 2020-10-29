2b51fec4bd0cf6aca632e7160654ebd962cbc2b4
Julien Lebot <julien.lebot@datadoghq.com>
2020-10-29 19:19:45 +0100

    Run from windows folder

--- windows/restore_packages.ps1
+++ windows/restore_packages.ps1
@@ -1,3 +1,4 @@
+cd windows
 Invoke-WebRequest -Uri https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -OutFile .\nuget.exe
 .\nuget.exe sources update -Name datadog -UserName (aws ssm get-parameter --region us-east-1 --name ci.datadog-agent-buildimages.chocolatey_username --with-decryption --query "Parameter.Value" --out text) -Password (aws ssm get-parameter --region us-east-1 --name ci.datadog-agent-buildimages.chocolatey_access_token --with-decryption --query "Parameter.Value" --out text)
 .\nuget.exe restore
