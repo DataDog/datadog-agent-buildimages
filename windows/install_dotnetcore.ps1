9d1aa1ed4180b26245c588da630fc8480e7edc90
Andrew Lock <andrew.lock@datadoghq.com>
2021-02-19 17:36:29 +0000

    Update .NET Core SDK to 5.0.103

--- windows/install_dotnetcore.ps1
+++ windows/install_dotnetcore.ps1
@@ -5,8 +5,8 @@ $ErrorActionPreference = 'Stop'
 # Script directory is $PSScriptRoot
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$Version = "3.1.403"
-$url = "https://download.visualstudio.microsoft.com/download/pr/38136cfe-04d4-4ce8-a8ea-369a800021df/08b29e05cd798d96b5987b417a989b80/dotnet-sdk-3.1.403-win-x64.exe"
+$Version = "5.0.103"
+$url = "https://download.visualstudio.microsoft.com/download/pr/674a9f7d-862e-4f92-91b6-f1cf3fed03ce/e07db4de77ada8da2c23261dfe9db138/dotnet-sdk-5.0.103-win-x64.exe"
 Write-Host -ForegroundColor Green "Installing dotnetcore from $($Url)"
 
 $out = "$($PSScriptRoot)\dotnetcoresdk.exe"
