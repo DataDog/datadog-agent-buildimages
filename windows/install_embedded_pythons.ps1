3551ffe3d1709d4e70d456d75eb9d65d3c1b87ab
Kylian Serrania <kylian.serrania@datadoghq.com>
2021-03-08 10:18:02 +0100

    [deb-armhf, rpm-armhf, windows] Update Python 2.7 pip url

--- windows/install_embedded_pythons.ps1
+++ windows/install_embedded_pythons.ps1
@@ -65,7 +65,7 @@ setx TEST_EMBEDDED_PY2 $py2Target
 setx TEST_EMBEDDED_PY3 $py3Target
 
 cd $py2Target
-curl https://bootstrap.pypa.io/2.7/get-pip.py -o get-pip.py
+curl https://bootstrap.pypa.io/pip/2.7/get-pip.py -o get-pip.py
 .\python get-pip.py pip==${Env:EMBEDDED_PIP_VERSION}
 
 
