fd4f38a9cc97d1b0a2f032809df8410c54534c3c
Julien Lebot <julien.ar.lebot@gmail.com>
2021-04-02 18:15:05 +0200

    Remove nuget & use vcpkg powershell file

---
+++ windows/install_vcpkg.ps1
@@ -0,0 +1,4 @@
+git clone https://github.com/microsoft/vcpkg
+.\vcpkg\scripts\bootstrap.ps1
+[Environment]::SetEnvironmentVariable("Path", [Environment]::GetEnvironmentVariable("Path", [EnvironmentVariableTarget]::Machine) + ";C:\vcpkg\", [System.EnvironmentVariableTarget]::Machine)
+vcpkg integrate install
