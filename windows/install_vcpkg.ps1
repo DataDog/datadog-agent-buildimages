6e9a762c341c30f74581d72a0d1d9773fb798a8c
Julien Lebot <julien.lebot@datadoghq.com>
2021-04-12 14:56:07 +0200

    Update windows/install_vcpkg.ps1
    Co-authored-by: Pablo Baeyens <pablo.baeyens@datadoghq.com>

--- windows/install_vcpkg.ps1
+++ windows/install_vcpkg.ps1
@@ -1,4 +1,4 @@
-git clone https://github.com/microsoft/vcpkg
+git clone --depth 1 https://github.com/microsoft/vcpkg
 .\vcpkg\scripts\bootstrap.ps1
 [Environment]::SetEnvironmentVariable("Path", [Environment]::GetEnvironmentVariable("Path", [EnvironmentVariableTarget]::Machine) + ";C:\vcpkg\", [System.EnvironmentVariableTarget]::Machine)
 .\vcpkg\vcpkg.exe integrate install
