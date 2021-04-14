83cca591c142c7ff09db17685769caa1121433c1
Julien Lebot <julien.lebot@datadoghq.com>
2021-04-14 14:20:08 +0200

    Fix paste error

--- windows/install_vcpkg.ps1
+++ windows/install_vcpkg.ps1
@@ -1,4 +1,4 @@
-git clone git clone --depth 1 --branch 2020.11 https://github.com/microsoft/vcpkg
+git clone --depth 1 --branch 2020.11 https://github.com/microsoft/vcpkg
 .\vcpkg\scripts\bootstrap.ps1
 [Environment]::SetEnvironmentVariable("Path", [Environment]::GetEnvironmentVariable("Path", [EnvironmentVariableTarget]::Machine) + ";C:\vcpkg\", [System.EnvironmentVariableTarget]::Machine)
 .\vcpkg\vcpkg.exe integrate install
