b14f431ac15b0389fa98001208ffd167a3d649c2
Julien Lebot <julien.lebot@datadoghq.com>
2021-04-22 12:27:25 +0200

    Use own vcpython27 (#130)

--- windows/install_vcpython.ps1
+++ windows/install_vcpython.ps1
@@ -6,12 +6,18 @@ $ErrorActionPreference = 'Stop'
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 
-$vcpmsi = 'https://download.microsoft.com/download/7/9/6/796EF2E4-801B-4FC4-AB28-B59FBF6D907B/VCForPython27.msi'
+$vcpmsi = 'https://s3.amazonaws.com/dd-agent-omnibus/VCForPython27.msi'
 
 Write-host -ForegroundColor Green Downloading VC++ for Python
 $out = "$($PSScriptRoot)\vcp.msi"
 
 (New-Object System.Net.WebClient).DownloadFile($vcpmsi, $out)
+
+if ((Get-FileHash -Algorithm SHA256 $out).Hash -ne "070474DB76A2E625513A5835DF4595DF9324D820F9CC97EAB2A596DCBC2F5CBF")
+{
+    Write-Error "VCForPython27.msi hash doesn't match"
+}
+
 Write-host -ForegroundColor Green VC++ for Python downloaded, installing...
 Start-Process msiexec -ArgumentList '/q /i vcp.msi' -Wait
 Remove-Item $out
