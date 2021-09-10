ec0c1ceb85dc4cf19d2b6e293673636ed26e9292
Kylian Serrania <kylian.serrania@datadoghq.com>
2021-09-10 11:06:43 +0200

    Update comments

--- setup_go.sh
+++ setup_go.sh
@@ -2,20 +2,21 @@
 
 set -ex
 
-# Install gimme to get go1.15
+# Install gimme to get go1.15.11
 curl -sL -o /bin/gimme https://raw.githubusercontent.com/travis-ci/gimme/master/gimme
 chmod +x /bin/gimme
 eval $(gimme 1.15.11)
 
 # Install gcc 4.9 (needed to compile go, as it introduces -fdiagnostics-color which is required
-# to compile go)
+# in the post-compilation steps of go)
+# We don't add it to the PATH so that it's not used in the Agent / rtloader builds
 apt-get update && apt-get install -y gcc-4.9-backport
 
 git clone https://go.googlesource.com/go goroot && cd goroot
 git checkout go$GO_VERSION
 cd src 
 
-# Use the bundled clang + go 1.15.11 to build the target go version
+# Use gcc 4.9 + go1.15.11 to build the target go version
 CC=/usr/lib/gcc-4.9-backport/bin/gcc ./all.bash
 
 # Update PATH to include the built go binaries
