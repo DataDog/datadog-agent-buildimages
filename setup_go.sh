fb9594e1a70ea52d4e2b5076b50ee2c198f99c83
Kylian Serrania <kylian.serrania@datadoghq.com>
2021-09-10 15:16:22 +0200

    Hack to make cgo work

--- setup_go.sh
+++ setup_go.sh
@@ -21,5 +21,12 @@ CC=/usr/lib/gcc-4.9-backport/bin/gcc ./all.bash
 # Remove gcc 4.9 after building go
 apt-get remove -y gcc-4.9-backport
 
+# HACK: cgo tries to look for gcc in the same place that CC
+# pointed to when go was compiled. By default, this is fine (since CC=gcc),
+# but here since CC is more specific, cgo will try to find gcc
+# in /usr/lib/gcc-4.9-backport/bin/gcc
+mkdir -p /usr/lib/gcc-4.9-backport/bin
+ln -s $(which gcc) /usr/lib/gcc-4.9-backport/bin/gcc
+
 # Update PATH to include the built go binaries
 echo 'export PATH="/goroot/bin:$PATH"' >> /root/.bashrc
