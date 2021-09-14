6f604f5f94a093b60870f8dbde5a7b47db6cfd8d
Kylian Serrania <kylian.serrania@datadoghq.com>
2021-09-14 15:38:02 +0200

    Add comments

--- setup_go.sh
+++ setup_go.sh
@@ -2,6 +2,11 @@
 
 set -ex
 
+# Installs go from source: https://golang.org/doc/install/source#go14 using the first documented option
+# (building go with a previous binary release of go).
+# Note: the official go build uses the fourth option (bootstrap the build using go1.4), but according to the documentation
+# this shouldn't make a difference.
+
 # Install gimme to get go1.15.11
 curl -sL -o /bin/gimme https://raw.githubusercontent.com/travis-ci/gimme/v1.5.4/gimme
 # Check sha256sum, fail otherwise
