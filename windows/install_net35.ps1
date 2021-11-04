578aa94ff60439ced922c810dd0f8376c18bfc25
Albert Vaca Cintora <albert.vaca@datadoghq.com>
2021-11-04 12:01:37 +0100

    Fix comment
    Co-authored-by: Kylian Serrania <kylian.serrania@datadoghq.com>

--- windows/install_net35.ps1
+++ windows/install_net35.ps1
@@ -45,7 +45,7 @@ $UpgradeTable = @{
 $kernelver = [int](get-itemproperty -path "hklm:software\microsoft\windows nt\currentversion" -name releaseid).releaseid
 $productname = (get-itemproperty -path "hklm:software\microsoft\windows nt\currentversion" -n productname).productname
 Write-Host -ForegroundColor Green "Detected kernel version $kernelver and product name $productname"
-// Windows Server 2022 still reports 2009 as releaseid
+# Windows Server 2022 still reports 2009 as releaseid
 if (($kernelver -eq 2009) -and ($productname.contains("Windows Server 2022"))) {
     $kernelver = 2022
 }
