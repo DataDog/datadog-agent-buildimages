9ce9016cfba75ebf6668719ceaa021976c5cb20e
Julien Lebot <julien.lebot@datadoghq.com>
2021-12-09 18:16:19 +0100

    Update install_vcpkg.ps1

--- windows/install_vcpkg.ps1
+++ windows/install_vcpkg.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = "Stop"
 
-git clone --depth 1 --branch 2020.11 https://github.com/microsoft/vcpkg
+git clone --depth 1 --branch 2021.05.12 https://github.com/microsoft/vcpkg
 .\vcpkg\scripts\bootstrap.ps1
 [Environment]::SetEnvironmentVariable("Path", [Environment]::GetEnvironmentVariable("Path", [EnvironmentVariableTarget]::Machine) + ";C:\vcpkg\", [System.EnvironmentVariableTarget]::Machine)
 .\vcpkg\vcpkg.exe integrate install
