a568fb64f08043d129fcf0393bd23fe5921cf03c
Julien Lebot <julien.lebot@datadoghq.com>
2021-12-14 16:28:44 +0100

    Update windows/install_vcpkg.ps1
    Co-authored-by: Pablo Baeyens <pablo.baeyens@datadoghq.com>

--- windows/install_vcpkg.ps1
+++ windows/install_vcpkg.ps1
@@ -1,5 +1,6 @@
 $ErrorActionPreference = "Stop"
 
+# Do not use '--depth 1' since vcpkg needs to browse its git history for dependency retrieval
 git clone --branch 2021.05.12 https://github.com/microsoft/vcpkg
 .\vcpkg\scripts\bootstrap.ps1
 [Environment]::SetEnvironmentVariable("Path", [Environment]::GetEnvironmentVariable("Path", [EnvironmentVariableTarget]::Machine) + ";C:\vcpkg\", [System.EnvironmentVariableTarget]::Machine)
