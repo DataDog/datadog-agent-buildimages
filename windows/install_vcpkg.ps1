ea7d46ce9e6d5a78f5dae111e6f66b88efe45ddd
Julien Lebot <julien.lebot@datadoghq.com>
2021-12-15 14:24:00 +0100

    Remove --depth 1 for vckpg install (#189)

--- windows/install_vcpkg.ps1
+++ windows/install_vcpkg.ps1
@@ -1,6 +1,7 @@
 $ErrorActionPreference = "Stop"
 
-git clone --depth 1 --branch 2021.05.12 https://github.com/microsoft/vcpkg
+# Do not use '--depth 1' since vcpkg needs to browse its git history for dependency retrieval
+git clone --branch 2021.05.12 https://github.com/microsoft/vcpkg
 .\vcpkg\scripts\bootstrap.ps1
 [Environment]::SetEnvironmentVariable("Path", [Environment]::GetEnvironmentVariable("Path", [EnvironmentVariableTarget]::Machine) + ";C:\vcpkg\", [System.EnvironmentVariableTarget]::Machine)
 .\vcpkg\vcpkg.exe integrate install
