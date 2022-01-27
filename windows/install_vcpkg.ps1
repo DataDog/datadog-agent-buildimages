323fb786700e5920d2a6c2f7fc4362a09a6a242b
Julien Lebot <julien.lebot@datadoghq.com>
2022-01-27 10:13:18 +0100

    Build vcpkg-tool from source (#195)

--- windows/install_vcpkg.ps1
+++ windows/install_vcpkg.ps1
@@ -2,6 +2,19 @@ $ErrorActionPreference = "Stop"
 
 # Do not use '--depth 1' since vcpkg needs to browse its git history for dependency retrieval
 git clone --branch 2021.05.12 https://github.com/microsoft/vcpkg
-.\vcpkg\scripts\bootstrap.ps1
+
+git clone https://github.com/microsoft/vcpkg-tool --branch 2022-01-19 C:\vcpkg-tool
+
+mkdir C:\vcpkg-build
+Push-Location C:\vcpkg-build
+cmake -DVCPKG_EMBED_GIT_SHA=ON -DVCPKG_BASE_VERSION=2022-01-19 C:\vcpkg-tool
+cmd /C "%VSTUDIO_ROOT%\VC\Auxiliary\Build\vcvars64.bat && msbuild /p:Configuration=Release vcpkg.sln"
+Move-Item C:\vcpkg-build\Release\vcpkg.exe c:\vcpkg\
+Pop-Location
+
+Remove-Item -Recurse -Force C:\vcpkg-tool
+Remove-Item -Recurse -Force C:\vcpkg-build
+
 [Environment]::SetEnvironmentVariable("Path", [Environment]::GetEnvironmentVariable("Path", [EnvironmentVariableTarget]::Machine) + ";C:\vcpkg\", [System.EnvironmentVariableTarget]::Machine)
+.\vcpkg\vcpkg.exe --version
 .\vcpkg\vcpkg.exe integrate install
