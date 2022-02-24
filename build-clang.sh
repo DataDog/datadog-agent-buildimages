a9dbb8cd911a99f9e63952d6b07fd50a556b09d6
Kylian Serrania <kylian.serrania@datadoghq.com>
2022-02-24 15:00:39 +0100

    [clang, cmake scripts] Check sha256 sums of downloaded artifacts

--- build-clang.sh
+++ build-clang.sh
@@ -3,8 +3,10 @@
 CLANG_VERSION=8.0.0
 
 curl -LO http://releases.llvm.org/${CLANG_VERSION}/llvm-${CLANG_VERSION}.src.tar.xz
+echo "8872be1b12c61450cacc82b3d153eab02be2546ef34fa3580ed14137bb26224c  llvm-${CLANG_VERSION}.src.tar.xz" | sha256sum --check
 tar -xf llvm-${CLANG_VERSION}.src.tar.xz
 curl -LO http://releases.llvm.org/${CLANG_VERSION}/cfe-${CLANG_VERSION}.src.tar.xz
+echo "084c115aab0084e63b23eee8c233abb6739c399e29966eaeccfc6e088e0b736b  llvm-${CLANG_VERSION}.src.tar.xz" | sha256sum --check
 tar -xf cfe-${CLANG_VERSION}.src.tar.xz
 mv cfe-${CLANG_VERSION}.src/ clang
 cd llvm-${CLANG_VERSION}.src
