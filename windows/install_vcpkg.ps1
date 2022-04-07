dc6167a20324d1cd4b57d6fe6c00a6a429e3101b
Julien Lebot <julien.lebot@datadoghq.com>
2022-04-07 10:50:53 +0200

    Use latest VCPKG, try using pre-built version

--- windows/install_vcpkg.ps1
+++ windows/install_vcpkg.ps1
@@ -1,19 +1,27 @@
 $ErrorActionPreference = "Stop"
 
+$buildVcpkgFromSource = $False
+# See https://github.com/microsoft/vcpkg/releases for updated version
+$vcpkgVersion = "2022.03.10"
+# See https://github.com/microsoft/vcpkg-tool/releases for updated version
+$vcpkgToolVersion = "2022-03-30"
+
 # Do not use '--depth 1' since vcpkg needs to browse its git history for dependency retrieval
-git clone --branch 2021.05.12 https://github.com/microsoft/vcpkg
+git clone --branch $vcpkgVersion https://github.com/microsoft/vcpkg
 
-git clone https://github.com/microsoft/vcpkg-tool --branch 2022-01-19 C:\vcpkg-tool
-
-mkdir C:\vcpkg-build
-Push-Location C:\vcpkg-build
-cmake -DVCPKG_EMBED_GIT_SHA=ON -DVCPKG_BASE_VERSION=2022-01-19 C:\vcpkg-tool
-cmd /C "%VSTUDIO_ROOT%\VC\Auxiliary\Build\vcvars64.bat && msbuild /p:Configuration=Release vcpkg.sln"
-Move-Item C:\vcpkg-build\Release\vcpkg.exe c:\vcpkg\
-Pop-Location
-
-Remove-Item -Recurse -Force C:\vcpkg-tool
-Remove-Item -Recurse -Force C:\vcpkg-build
+if ($buildVcpkgFromSource) {
+    git clone https://github.com/microsoft/vcpkg-tool --branch $vcpkgToolVersion C:\vcpkg-tool
+    mkdir C:\vcpkg-build
+    Push-Location C:\vcpkg-build
+    cmake -DVCPKG_EMBED_GIT_SHA=ON -DVCPKG_BASE_VERSION=$vcpkgToolVersion C:\vcpkg-tool
+    cmd /C "%VSTUDIO_ROOT%\VC\Auxiliary\Build\vcvars64.bat && msbuild /p:Configuration=Release vcpkg.sln"
+    Move-Item C:\vcpkg-build\Release\vcpkg.exe c:\vcpkg\
+    Pop-Location
+    Remove-Item -Recurse -Force C:\vcpkg-tool
+    Remove-Item -Recurse -Force C:\vcpkg-build
+} else {
+    .\vcpkg\scripts\bootstrap.ps1
+}
 
 [Environment]::SetEnvironmentVariable("Path", [Environment]::GetEnvironmentVariable("Path", [EnvironmentVariableTarget]::Machine) + ";C:\vcpkg\", [System.EnvironmentVariableTarget]::Machine)
 .\vcpkg\vcpkg.exe --version
