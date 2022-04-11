dfc7750f76243b754297f4835dd82aa440aa8efb
Derek Brown <derek.brown@datadoghq.com>
2022-04-11 15:48:17 -0700

    Update vcpkg (#226)
    * try updating vcpkg
    
    * try again

--- windows/install_vcpkg.ps1
+++ windows/install_vcpkg.ps1
@@ -1,13 +1,13 @@
 $ErrorActionPreference = "Stop"
 
 # Do not use '--depth 1' since vcpkg needs to browse its git history for dependency retrieval
-git clone --branch 2021.05.12 https://github.com/microsoft/vcpkg
+git clone --branch 2022.03.10 https://github.com/microsoft/vcpkg
 
-git clone https://github.com/microsoft/vcpkg-tool --branch 2022-01-19 C:\vcpkg-tool
+git clone https://github.com/microsoft/vcpkg-tool --branch 2022-03-30 C:\vcpkg-tool
 
 mkdir C:\vcpkg-build
 Push-Location C:\vcpkg-build
-cmake -DVCPKG_EMBED_GIT_SHA=ON -DVCPKG_BASE_VERSION=2022-01-19 C:\vcpkg-tool
+cmake -DVCPKG_EMBED_GIT_SHA=ON -DVCPKG_BASE_VERSION=2022-03-30 C:\vcpkg-tool
 cmd /C "%VSTUDIO_ROOT%\VC\Auxiliary\Build\vcvars64.bat && msbuild /p:Configuration=Release vcpkg.sln"
 Move-Item C:\vcpkg-build\Release\vcpkg.exe c:\vcpkg\
 Pop-Location
