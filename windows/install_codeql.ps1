364c60f3b4fab49bed1d89178fb043df699db948
Derek Brown <derek.brown@datadoghq.com>
2022-08-29 17:30:44 -0700

    add hash check for codeql zip

--- windows/install_codeql.ps1
+++ windows/install_codeql.ps1
@@ -10,6 +10,11 @@ Write-Host -ForegroundColor Green "Downloading $codeqlzip to $out"
 
 (New-Object System.Net.WebClient).DownloadFile($codeqlzip, $out)
 
+if ((Get-FileHash -Algorithm SHA256 $out).Hash -ne "$ENV:CODEQL_HASH") { 
+    Write-Host \"Wrong hashsum for ${out}: got '$((Get-FileHash -Algorithm SHA256 $out).Hash)', expected '$ENV:CODEQL_HASH'.\"; 
+    exit 1 
+}
+
 Write-Host -ForegroundColor Green "Extracting $out to c:\"
 
 Start-Process "7z" -ArgumentList "x -oc:\ $out" -Wait
