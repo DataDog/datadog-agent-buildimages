8bb691bfbdf4a1df6892dac0d2a2aaa7ede80875
Derek Brown <derek.brown@datadoghq.com>
2022-09-04 00:03:19 -0700

    remove ridk enable; fixes calls to cmd

--- windows/helpers/install_vcpkg.ps1
+++ windows/helpers/install_vcpkg.ps1
@@ -18,7 +18,6 @@ git clone https://github.com/microsoft/vcpkg-tool --branch 2022-03-30 C:\vcpkg-t
 mkdir C:\vcpkg-build
 Push-Location C:\vcpkg-build
 cmake -DVCPKG_EMBED_GIT_SHA=ON -DVCPKG_BASE_VERSION=2022-03-30 C:\vcpkg-tool
-Get-Command cmd -all
 cmd /C "%VSTUDIO_ROOT%\VC\Auxiliary\Build\vcvars64.bat && msbuild /p:Configuration=Release vcpkg.sln"
 
 Move-Item C:\vcpkg-build\Release\vcpkg.exe c:\vcpkg\
