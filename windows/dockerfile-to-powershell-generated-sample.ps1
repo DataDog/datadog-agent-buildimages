486ab2146813275de73c2678205eff219d60206f
Dustin J. Mitchell <dustin.mitchell@datadoghq.com>
2022-11-03 12:53:59 -0400

    Update to go1.18.8 (#314)

--- windows/dockerfile-to-powershell-generated-sample.ps1
+++ windows/dockerfile-to-powershell-generated-sample.ps1
@@ -116,8 +116,8 @@ function Update-SessionEnvironment {
 # Content: ENV VS2017BUILDTOOLS_DOWNLOAD_URL "https://download.visualstudio.microsoft.com/download/pr/9b3476ff-6d0a-4ff8-956d-270147f21cd4/ccfb9355f4f753315455542f966025f96de734292d3908c8c3717e9685b709f0/vs_BuildTools.exe"
 [System.Environment]::SetEnvironmentVariable('VS2017BUILDTOOLS_DOWNLOAD_URL','https://download.visualstudio.microsoft.com/download/pr/9b3476ff-6d0a-4ff8-956d-270147f21cd4/ccfb9355f4f753315455542f966025f96de734292d3908c8c3717e9685b709f0/vs_BuildTools.exe')
 
-# Content: ENV GO_VERSION "1.18.6"
-[System.Environment]::SetEnvironmentVariable('GO_VERSION','1.18.6')
+# Content: ENV GO_VERSION "1.18.8"
+[System.Environment]::SetEnvironmentVariable('GO_VERSION','1.18.8')
 
 # Content: ENV GO_SHA256 "480B7212AB1D152D5E3FC382AC34D3DD26BF637AE4537C35B4B554EDE8A36B47"
 [System.Environment]::SetEnvironmentVariable('GO_SHA256','480B7212AB1D152D5E3FC382AC34D3DD26BF637AE4537C35B4B554EDE8A36B47')
@@ -216,7 +216,7 @@ function Update-SessionEnvironment {
 # LABEL resolved: vs2017buildtools_version='16.8.3.0'
 
 # Content: LABEL go_version=${GO_VERSION}
-# LABEL resolved: go_version='1.18.6'
+# LABEL resolved: go_version='1.18.8'
 
 # Content: LABEL ruby_version=${RUBY_VERSION}
 # LABEL resolved: ruby_version='2.6.6-1'
