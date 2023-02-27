964d4c191d0c25c4d4301e6998e04b190ef77af7
Florent Clarret <florent.clarret@datadoghq.com>
2023-02-27 13:17:15 +0100

    drop librdkafka from windows

--- windows/helpers/phase4/install_librdkafka.ps1
+++
@@ -1,9 +0,0 @@
-param (
-    [Parameter(Mandatory=$true)][string]$Version
-)
-
-Write-Host -ForegroundColor Green "Installing librdkafka $Version"
-
-c:\vcpkg\vcpkg.exe install --x-wait-for-lock  "--x-manifest-root=..\vcpkg\librdkafka\" "--x-install-root=C:\vcpkg\scripts\buildsystems\msbuild\..\..\..\installed\\" --feature-flags=versions
-
-Write-Host -ForegroundColor Green "Installed librdkafka $Version"
