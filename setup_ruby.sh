0d6f9c260e3c0ea3a5ae285c065aed338582cbc0
Paul Cacheux <paul.cacheux@datadoghq.com>
2023-03-09 13:12:58 +0100

    fix install script

--- setup_ruby.sh
+++ setup_ruby.sh
@@ -4,31 +4,32 @@ set -ex
 
 RUBY_MAJOR=2.7
 RUBY_VERSION=2.7.7
-
-RUN apt-get -y update && apt-get -y install curl make gcc libssl-dev
+RUBY_SHA256="e10127db691d7ff36402cfe88f418c8d025a3f1eea92044b162dd72f0b8c7b90"
 
 curl -OL https://cache.ruby-lang.org/pub/ruby/$RUBY_MAJOR/ruby-$RUBY_VERSION.tar.gz
+echo "$RUBY_SHA256  ruby-$RUBY_VERSION.tar.gz" | sha256sum --check
 tar -xzf ruby-$RUBY_VERSION.tar.gz
 rm -rf ruby-$RUBY_VERSION.tar.gz
 
-CONFIGURE_ARGS=""
+CONFIGURE_ARGS="--disable-install-doc --enable-shared"
+CFLAGS="-O3"
 
 if [[ -z "$SKIP_CONDA_SSL" ]]; then
     CONFIGURE_ARGS="$CONFIGURE_ARGS --with-openssl-dir='$CONDA_PATH'"
 fi
 
 if [[ -n "$RUBY_WITH_ARCH" ]]; then
-    CONFIGURE_ARGS="$CONFIGURE_ARGS --with-arch='$RUBY_WITH_ARCH'"
+    CFLAGS="$CFLAGS -march=$RUBY_WITH_ARCH"
 fi
 
 if [[ -n "$RUBY_BUILD_ARCH" ]]; then
-    CONFIGURE_ARGS="$CONFIGURE_ARGS -C '--build' -C '$RUBY_BUILD_ARCH'"
+    CONFIGURE_ARGS="$CONFIGURE_ARGS --build $RUBY_BUILD_ARCH"
 fi
 
 mkdir -p ruby-$RUBY_VERSION/build
 
 pushd ruby-$RUBY_VERSION/build
-../configure $CONFIGURE_ARGS
+CFLAGS="$CFLAGS" CCFLAGS="$CFLAGS" CXXFLAGS="$CFLAGS" ../configure $CONFIGURE_ARGS
 make install
 popd
 
