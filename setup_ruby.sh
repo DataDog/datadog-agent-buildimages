a77bebb078d5714bdbece844ff9c6cf7d356b3c1
Paul Cacheux <paul.cacheux@datadoghq.com>
2023-03-09 12:56:27 +0100

    convert more images

--- setup_ruby.sh
+++ setup_ruby.sh
@@ -11,10 +11,16 @@ curl -OL https://cache.ruby-lang.org/pub/ruby/$RUBY_MAJOR/ruby-$RUBY_VERSION.tar
 tar -xzf ruby-$RUBY_VERSION.tar.gz
 rm -rf ruby-$RUBY_VERSION.tar.gz
 
+CONFIGURE_ARGS=""
+
+if [[ -z "$SKIP_CONDA_SSL" ]]; then
+    CONFIGURE_ARGS="$CONFIGURE_ARGS --with-openssl-dir=$CONDA_PATH"
+fi
+
 mkdir -p ruby-$RUBY_VERSION/build
 
 pushd ruby-$RUBY_VERSION/build
-../configure --with-openssl-dir="$CONDA_PATH"
+../configure $CONFIGURE_ARGS
 make install
 popd
 
