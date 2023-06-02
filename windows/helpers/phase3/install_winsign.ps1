21b922f5262f3354b4317cc10bdc95b9357e4457
Derek Brown <derek.brown@datadoghq.com>
2023-06-01 18:30:04 -0700

    Update wincerts to have allowable filename for installing the wcs wheel
    Pin the boto3 to work around the boto3 issue.

--- windows/helpers/phase3/install_winsign.ps1
+++ windows/helpers/phase3/install_winsign.ps1
@@ -4,8 +4,9 @@ $ProgressPreference = 'SilentlyContinue'
 Write-Host -ForegroundColor Green "Installing Windows Codesign Helper $ENV:WINSIGN_VERSION"
 
 ## need to have more rigorous download at some point, but
-$codesign_wheel = "https://s3.amazonaws.com/dd-agent-omnibus/windows-code-signer/windows_code_signer-$($ENV:WINSIGN_VERSION)-py3-none-any.whl"
-$codesign_wheel_target = "c:\devtools\windows_code_signer.whl"
+$codesign_base = "windows_code_signer-$($ENV:WINSIGN_VERSION)-py3-none-any.whl"
+$codesign_wheel = "https://s3.amazonaws.com/dd-agent-omnibus/windows-code-signer/$($codesign_base)"
+$codesign_wheel_target = "c:\devtools\$($codesign_base)"
 (New-Object System.Net.WebClient).DownloadFile($codesign_wheel, $codesign_wheel_target)
 
 Get-RemoteFile -RemoteFile $codesign_wheel -LocalFile $codesign_wheel_target -VerifyHash $ENV:WINSIGN_SHA256
