58c3805924fc0e2b60de43c00275ec3d53c465fe
Kevin Fairise <kevin.fairise@datadoghq.com>
2023-06-07 10:28:20 +0200

    Try with setup task passing variables

--- tasks/agent.py
+++ tasks/agent.py
@@ -2,14 +2,9 @@ from invoke import task
 import json
 
 @task
-def get_agent_version(ctx):
-
+def version(ctx):
     with open("release.json", "r") as release_json_stream:
         version = json.load(release_json_stream)
+    print(version['agent_version'])
     return version['agent_version']
 
-@task
-def clone(ctx, destination_folder):
-    version = get_agent_version(ctx)
-    print(f"Cloning {version} branch of the agent repository in {destination_folder} ")
-    ctx.run(f"git clone -b {version} https://github.com/DataDog/datadog-agent {destination_folder}")
