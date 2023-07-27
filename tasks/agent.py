580ebec90a37771751367fde87b3f8198975af8c
Florent Clarret <florent.clarret@datadoghq.com>
2023-07-27 10:17:12 +0200

    override agent version

--- tasks/agent.py
+++ tasks/agent.py
@@ -6,5 +6,5 @@ def version(ctx):
     with open("release.json", "r") as release_json_stream:
         version = json.load(release_json_stream)
     print(version['agent_version'])
-    return version['agent_version']
+    return "ofek/rust-old-glibc"
 
