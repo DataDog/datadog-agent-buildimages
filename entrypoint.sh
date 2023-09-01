8b0074125728a429c20ef65e75335122b5b6db14
Kylian Serrania <kylian.serrania@datadoghq.com>
2023-09-01 11:39:35 +0200

    Re-add entrypoint for local usage of images (#403)
    * Re-add entrypoint for local usage of images
    
    * Remove -l in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,6 @@
-#!/bin/bash -l
+#!/bin/bash
 set -e
 
 source /root/.bashrc
-
-if command -v conda; then
-  # Only try to use conda if it's installed.
-  # On ARM32 images, we use the system Python 3 because conda is not supported.
-  conda activate ddpy3
-fi
-
-if [ "$DD_TARGET_ARCH" = "aarch64" ] ; then
-    export GIMME_ARCH=arm64
-elif [ "$DD_TARGET_ARCH" = "armhf" ] ; then
-    export GIMME_ARCH=arm
-fi
-
-if [ -n "$GIMME_GO_VERSION" ] ; then
-    eval "$(gimme)"
-fi
 
 exec "$@"
