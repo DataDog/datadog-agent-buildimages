e68e3dcc1dd25ff37774fb07c5ae9dee267adc45
Pythyu <antoine.zellmeyer@datadoghq.com>
2023-10-27 13:54:39 +0200

    feat(gcr): moved images to different folder

--- tasks/pipeline.py
+++ tasks/pipeline.py
@@ -36,11 +36,11 @@ def trigger_child_pipeline(ctx: Context, variable):
         print("CI_JOB_TOKEN environment variable is undefined and required.")
         return
     metadata = _trigger_pipeline(ctx, variable, CI_JOB_TOKEN)
-    pipeline_status = _get_pipeline_status(ctx, metadata["id"], CI_JOB_TOKEN)
+    pipeline_status = _get_pipeline_status(ctx, metadata["id"], os.getenv("GITLAB_TOKEN"))
     while pipeline_status not in ["success", "failed", "canceled", "skipped"]:
         print(f"Current status : {pipeline_status}...")
         time.sleep(15)
-        pipeline_status = _get_pipeline_status(ctx, metadata["id"], CI_JOB_TOKEN)
+        pipeline_status = _get_pipeline_status(ctx, metadata["id"], os.getenv("GITLAB_TOKEN"))
     print(f"Pipeline {metadata['id']} ended with {pipeline_status} status.")
     if pipeline_status != "success":
         raise Exit(
