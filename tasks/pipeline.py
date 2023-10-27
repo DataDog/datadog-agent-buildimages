011aa88583656aaa40058dcbcd71cf6a194e9e01
Pythyu <antoine.zellmeyer@datadoghq.com>
2023-10-27 08:00:46 +0200

    feat(trigger_child_pipeline): follow child pipeline status

--- tasks/pipeline.py
+++ tasks/pipeline.py
@@ -1,21 +1,48 @@
 from invoke import task
 from invoke.exceptions import Exit
+from invoke.context import Context
+import time
 import os
 
-@task
-def trigger_child_pipeline(ctx, variable):
-    CI_JOB_TOKEN = os.getenv("CI_JOB_TOKEN")
-    if not CI_JOB_TOKEN:
-        print("CI_JOB_TOKEN environment variable is undefined and required.")
-        return
 
+def _trigger_pipeline(ctx: Context, variable, job_token):
     forms = ""
     for key in variable.split(","):
         env_value = os.getenv(key)
         if env_value:
             forms += f"--form variables[{key}]={env_value} "
-    forms += f"--form token={CI_JOB_TOKEN} "
+    forms += f"--form token={job_token} "
     forms += "--form ref=main "
     trigger_cmd = f"curl --request POST {forms} https://gitlab.ddbuild.io/api/v4/projects/1856/trigger/pipeline"
-    err = ctx.run(trigger_cmd)
+    output = ctx.run(trigger_cmd)
+    if output.exited != 0:
+        raise Exit("An error occurred while creating the pipeline", code=1)
+    return dict(output.stdout)
 
+
+def _get_pipeline_status(ctx: Context, pipeline_id, job_token):
+    get_status_cmd = f"curl --request GET --form token={job_token} https://gitlab.ddbuild.io/api/v4/projects/1856/pipelines/{pipeline_id}"
+    output = ctx.run(get_status_cmd)
+    if output.exited != 0:
+        raise Exit("An error occurred while creating the pipeline", code=1)
+    return dict(output.stdout)["status"]
+
+
+@task
+def trigger_child_pipeline(ctx: Context, variable):
+    CI_JOB_TOKEN = os.getenv("CI_JOB_TOKEN")
+    if not CI_JOB_TOKEN:
+        print("CI_JOB_TOKEN environment variable is undefined and required.")
+        return
+    metadata = _trigger_pipeline(ctx, variable, CI_JOB_TOKEN)
+    pipeline_status = _get_pipeline_status(ctx, metadata["id"], CI_JOB_TOKEN)
+    while pipeline_status not in ["success", "failed", "canceled", "skipped"]:
+        print(f"Current status : {pipeline_status}...")
+        time.sleep(15)
+        pipeline_status = _get_pipeline_status(ctx, metadata["id"], CI_JOB_TOKEN)
+    print(f"Pipeline {metadata['id']} ended with {pipeline_status} status.")
+    if pipeline_status != "success":
+        raise Exit(
+            f"Pipeline {metadata['id']} failed. Please look at {metadata['web_url']} for more details.",
+            code=1,
+        )
