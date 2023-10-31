5fda427773ff9f862bd491fe96886630396d8358
Pythyu <antoine.zellmeyer@datadoghq.com>
2023-10-31 12:14:23 +0100

    feat(GITLAB_TOKEN): fetch GITLAB_TOKEN from ssm

--- tasks/pipeline.py
+++ tasks/pipeline.py
@@ -22,7 +22,7 @@ def _trigger_pipeline(ctx: Context, variable, job_token):
 
 
 def _get_pipeline_status(ctx: Context, pipeline_id, job_token):
-    get_status_cmd = f"curl --header \"JOB-TOKEN: {job_token}\" https://gitlab.ddbuild.io/api/v4/projects/1856/pipelines/{pipeline_id}"
+    get_status_cmd = f"curl --header \"PRIVATE-TOKEN: {job_token}\" https://gitlab.ddbuild.io/api/v4/projects/1856/pipelines/{pipeline_id}"
     output = ctx.run(get_status_cmd)
     if output.exited != 0:
         raise Exit("An error occurred while creating the pipeline", code=1)
@@ -32,15 +32,16 @@ def _get_pipeline_status(ctx: Context, pipeline_id, job_token):
 @task
 def trigger_child_pipeline(ctx: Context, variable):
     CI_JOB_TOKEN = os.getenv("CI_JOB_TOKEN")
-    if not CI_JOB_TOKEN:
-        print("CI_JOB_TOKEN environment variable is undefined and required.")
+    GITLAB_TOKEN = os.getenv("GITLAB_TOKEN")
+    if not CI_JOB_TOKEN or not GITLAB_TOKEN:
+        print("CI_JOB_TOKEN and GITLAB_TOKEN environment variable is undefined and required.")
         return
     metadata = _trigger_pipeline(ctx, variable, CI_JOB_TOKEN)
-    pipeline_status = _get_pipeline_status(ctx, metadata["id"], CI_JOB_TOKEN)
+    pipeline_status = _get_pipeline_status(ctx, metadata["id"], GITLAB_TOKEN)
     while pipeline_status not in ["success", "failed", "canceled", "skipped"]:
         print(f"Current status : {pipeline_status}...")
         time.sleep(15)
-        pipeline_status = _get_pipeline_status(ctx, metadata["id"], CI_JOB_TOKEN)
+        pipeline_status = _get_pipeline_status(ctx, metadata["id"], GITLAB_TOKEN)
     print(f"Pipeline {metadata['id']} ended with {pipeline_status} status.")
     if pipeline_status != "success":
         raise Exit(
