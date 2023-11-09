9ef99f936d094bc5a8466cf69e3ac80b0b0b524a
Pythyu <antoine.zellmeyer@datadoghq.com>
2023-11-09 14:42:42 +0100

    feat(wait_for_trigger): fix typo

--- tasks/pipeline.py
+++ tasks/pipeline.py
@@ -1,10 +1,10 @@
 from invoke import task
-from invoke.exception import Exit
+from invoke.exceptions import Exit
 import json
 
 @task
 def check_pipeline_status(ctx):
-    jobs = ctx.run("curl --header \"JOB-TOKEN: $CI_JOB_TOKEN\" https://gitlab.ddbuild.io/api/v4/projects/291/pipelines/${CI_PIPELINE_ID}/jobs?scope[]=failed",hide=True)
+    jobs = ctx.run("curl --header \"JOB-TOKEN: $CI_JOB_TOKEN\" \"https://gitlab.ddbuild.io/api/v4/projects/291/pipelines/${CI_PIPELINE_ID}/jobs?scope[]=failed\"",hide=True)
     jobsList = json.loads(jobs.stdout)
     if len(jobsList) > 0:
         raise Exit("Trigger tests failed !")
