6aa58476508f5b06615383bd18f6cdadb2b5e3f9
Pythyu <antoine.zellmeyer@datadoghq.com>
2023-11-10 09:59:24 +0100

    feat(wait_for_trigger): test success

--- tasks/pipeline.py
+++ tasks/pipeline.py
@@ -7,5 +7,6 @@ def check_pipeline_status(ctx):
     jobs = ctx.run("curl --header \"JOB-TOKEN: $CI_JOB_TOKEN\" \"https://gitlab.ddbuild.io/api/v4/projects/291/pipelines/${CI_PIPELINE_ID}/jobs?scope[]=failed\"",hide=True)
     jobsList = json.loads(jobs.stdout)
     if len(jobsList) > 0:
+        print(jobsList)
         raise Exit("Trigger tests failed !")
     return True
