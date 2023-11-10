59c38eb702e1368e27b45f2c847b39e93d5c33c5
Hugo Beauzée-Luyssen <hugo.beauzee@datadoghq.com>
2023-11-10 10:36:44 +0100

    debug

--- tasks/pipeline.py
+++ tasks/pipeline.py
@@ -6,7 +6,9 @@ import os
 
 @task
 def check_pipeline_status(ctx):
-    jobs = ctx.run(f"curl --header \"JOB-TOKEN: {os.environ['CI_JOB_TOKEN']}\" \"{os.environ['CI_API_V4_URL']}/projects/{os.environ['CI_PROJECT_ID']}/pipelines/{os.environ['CI_PIPELINE_ID']}/jobs?scope[]=failed\"", hide=True)
+    url = f"{os.environ['CI_API_V4_URL']}/projects/{os.environ['CI_PROJECT_ID']}/pipelines/{os.environ['CI_PIPELINE_ID']}/jobs?scope[]=failed"
+    print(f'querying {url}')
+    jobs = ctx.run(f"curl --header \"JOB-TOKEN: {os.environ['CI_JOB_TOKEN']}\" \"{url}\"", hide=True)
     jobsList = json.loads(jobs.stdout)
     if len(jobsList) > 0:
         print(jobsList)
