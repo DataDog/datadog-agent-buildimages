462525fa3982c933a36f42316272a019f5b43e90
Hugo Beauzée-Luyssen <hugo.beauzee@datadoghq.com>
2023-11-10 10:46:01 +0100

    attempt to use PRIVATE-TOKEN

--- tasks/pipeline.py
+++ tasks/pipeline.py
@@ -7,8 +7,7 @@ import os
 @task
 def check_pipeline_status(ctx):
     url = f"{os.environ['CI_API_V4_URL']}/projects/{os.environ['CI_PROJECT_ID']}/pipelines/{os.environ['CI_PIPELINE_ID']}/jobs?scope[]=failed"
-    print(f'querying {url}')
-    jobs = ctx.run(f"curl --header \"JOB-TOKEN: {os.environ['CI_JOB_TOKEN']}\" \"{url}\"", hide=True)
+    jobs = ctx.run(f"curl --header \"PRIVATE-TOKEN: {os.environ['CI_JOB_TOKEN']}\" \"{url}\"", hide=True)
     jobsList = json.loads(jobs.stdout)
     if len(jobsList) > 0:
         print(jobsList)
