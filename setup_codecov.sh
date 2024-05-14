df4f171a9171149a6e177fcb9f20709d81cb79be
Alexandre Menasria <alexandre.menasria@datadoghq.com>
2024-05-14 15:28:38 +0200

    [Codecov] Install aarch64 codecov on arm runners

--- setup_codecov.sh
+++ setup_codecov.sh
@@ -4,11 +4,34 @@ set -euo pipefail
 
 CODECOV_VERSION=0.6.1
 
+# Check if the architecture was provided
+if [ -z "$1" ]; then
+  echo "Usage: $0 <architecture>"
+  echo "Example: $0 arm64"
+  exit 1
+fi
+
+ARCH=$1
+
+case $ARCHITECTURE in
+  arm64)
+    CODECOV_ARCH="aarch64"
+    ;;
+  amd64)
+    CODECOV_ARCH="linux"
+    ;;
+  *)
+    echo "Invalid architecture: $ARCHITECTURE"
+    echo "Supported architectures are: arm64, amd64"
+    exit 1
+    ;;
+esac
+
 # Integrity checking the uploader
 curl https://keybase.io/codecovsecurity/pgp_keys.asc | gpg --no-default-keyring --keyring trustedkeys.gpg --import
-curl -Os https://uploader.codecov.io/v${CODECOV_VERSION}/linux/codecov
-curl -Os https://uploader.codecov.io/v${CODECOV_VERSION}/linux/codecov.SHA256SUM
-curl -Os https://uploader.codecov.io/v${CODECOV_VERSION}/linux/codecov.SHA256SUM.sig
+curl -Os https://uploader.codecov.io/v${CODECOV_VERSION}/${CODECOV_ARCH}/codecov
+curl -Os https://uploader.codecov.io/v${CODECOV_VERSION}/${CODECOV_ARCH}/codecov.SHA256SUM
+curl -Os https://uploader.codecov.io/v${CODECOV_VERSION}/${CODECOV_ARCH}/codecov.SHA256SUM.sig
 gpgv codecov.SHA256SUM.sig codecov.SHA256SUM
 shasum -a 256 -c codecov.SHA256SUM
 rm codecov.SHA256SUM.sig codecov.SHA256SUM
