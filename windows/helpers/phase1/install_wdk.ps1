e4b5ba3845474ba38f77eb381feb592cc438736f
Julien Lebot <julien.lebot@datadoghq.com>
2024-07-10 13:34:32 +0200

    Change domain name

--- windows/helpers/phase1/install_wdk.ps1
+++ windows/helpers/phase1/install_wdk.ps1
@@ -15,7 +15,7 @@ function Install-MissingBuildTasks() {
     }
     # Avoid NuGet rate limits by using cached package in our S3 bucket
     # Get-RemoteFile -RemoteFile "https://www.nuget.org/api/v2/package/Microsoft.Windows.WDK.x64/10.0.26100.1" -LocalFile "c:\tmp\Microsoft.Windows.WDK.x64.10.0.26100.1.nupkg" -VerifyHash "247B2919AE451F65BA5F1CD51C7C39730FB0FC383D607F3E8AB317FDDC8A8239"
-    Get-RemoteFile -RemoteFile "https://dd-agent-omnibus.s3.amazonaws.com/microsoft.windows.wdk.x64.10.0.26100.1.nupkg" -LocalFile "c:\tmp\Microsoft.Windows.WDK.x64.10.0.26100.1.nupkg" -VerifyHash "247B2919AE451F65BA5F1CD51C7C39730FB0FC383D607F3E8AB317FDDC8A8239"
+    Get-RemoteFile -RemoteFile "https://s3.amazonaws.com/dd-agent-omnibus/microsoft.windows.wdk.x64.10.0.26100.1.nupkg" -LocalFile "c:\tmp\Microsoft.Windows.WDK.x64.10.0.26100.1.nupkg" -VerifyHash "247B2919AE451F65BA5F1CD51C7C39730FB0FC383D607F3E8AB317FDDC8A8239"
     Start-Process "7z" -ArgumentList "x -oc:\tmp c:\tmp\microsoft.windows.wdk.x64.10.0.26100.1.nupkg" -wait
     Copy-Item "c:\tmp\c\build\10.0.26100.0\bin\Microsoft.DriverKit.Build.Tasks.17.0.dll" "C:\Program Files (x86)\Windows Kits\10\build\bin\"
     Copy-Item "c:\tmp\c\build\10.0.26100.0\bin\Microsoft.DriverKit.Build.Tasks.PackageVerifier.17.0.dll" "C:\Program Files (x86)\Windows Kits\10\build\bin\"
