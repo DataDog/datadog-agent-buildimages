483061dfbc73dc25c5667f8b4bd4bb47ad01c99a
Branden Clark <branden.clark@datadoghq.com>
2024-07-31 15:08:28 -0400

    remove ruby from Machine PATH

--- windows/helpers/phase2/install_ruby.ps1
+++ windows/helpers/phase2/install_ruby.ps1
@@ -23,10 +23,12 @@ $out = "$($PSScriptRoot)\rubyinstaller.exe"
 Get-RemoteFile -RemoteFile $rubyexe -LocalFile $out -VerifyHash $Sha256
 
 
+# pass nomodpath to avoid putting ruby at the front of the Machine PATH
+# we'll add it to the end of the User PATH ourselves
 Write-Host -ForegroundColor Green Done downloading Ruby, installing
-Start-Process $out -ArgumentList '/verysilent /dir="c:\tools\ruby" /tasks="assocfiles,noridkinstall,modpath"' -Wait
+Start-Process $out -ArgumentList '/verysilent /dir="c:\tools\ruby" /tasks="assocfiles,noridkinstall,nomodpath"' -Wait
 
-Add-ToPath -NewPath "c:\tools\ruby\bin" -Local
+Add-ToPath -NewPath "c:\tools\ruby\bin" -Local -Global
 Add-EnvironmentVariable -Variable RIDK -Value ((Get-Command ridk).Path) -Global
 Start-Process gem -ArgumentList  'install bundler -v 2.3.24' -Wait
 
