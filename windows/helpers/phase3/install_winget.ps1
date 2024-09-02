2c8db38a233c463a2206aa1aebfbf1545406faf9
Julien Lebot <julien.lebot@datadoghq.com>
2024-09-02 11:54:26 +0200

    Update wingetcreate to latest version and install dotnet runtime 6 (#646)

--- windows/helpers/phase3/install_winget.ps1
+++ windows/helpers/phase3/install_winget.ps1
@@ -3,7 +3,6 @@ param (
     [Parameter(Mandatory=$true)][string]$Sha256
 )
 
-
 $wingetexe="https://github.com/microsoft/winget-create/releases/download/v$($Version)/wingetcreate.exe"
 
 $isInstalled, $isCurrent = Get-InstallUpgradeStatus -Component "winget" -Keyname "version" -TargetValue $Version
@@ -29,3 +28,32 @@ Remove-Item $out
 Add-ToPath -NewPath "c:\winget" -Local -Global
 Set-InstalledVersionKey -Component "winget" -Keyname "version" -TargetValue $Version
 Write-Host -ForegroundColor Green Done with Winget
+
+# Dotnet 6.0 **runtime** is necessary for wingetcreate, install it here
+. .\helpers.ps1
+
+$dotnetcore6url = "https://download.visualstudio.microsoft.com/download/pr/3c01bbe6-a49d-468f-8335-f195588f582f/b935469e8480e611eae4d79b2e51965e/dotnet-runtime-6.0.33-win-x64.exe"
+$dotnetcore6hash = "DDF16712E509CC7575DB52CE116B004B270538646A098CBC51AB38E9E9CB45E1"
+$isInstalled, $isCurrent = Get-InstallUpgradeStatus -Component "dotnet" -Keyname "DownloadFile" -TargetValue $dotnetcore6url
+if($isInstalled -and $isCurrent){
+    Write-Host -ForegroundColor Green ".NET Core 6 Up to date"
+    return
+}
+if(-not $isCurrent){
+    Write-Host -ForegroundColor Yellow "Attempting to install .NET Core 6"
+    ## presumably executable knows how to handle upgrade
+}
+Write-Host -ForegroundColor Green "Installing dotnet core 6 from $dotnetcore6url"
+$out = "$($PSScriptRoot)\dotnetcore.exe"
+
+Get-RemoteFile -RemoteFile $dotnetcore6url -LocalFile $out -VerifyHash $dotnetcore6hash
+Write-Host -ForegroundColor Green Downloading $dotnetcore6url to $out
+start-process -FilePath $out -ArgumentList "/install /quiet /norestart" -wait
+Remove-Item $out
+Reload-Path
+
+# Print out installed runtimes for visual inspection
+dotnet --list-runtimes
+
+# Test wingetcreate to ensure it works
+wingetcreate.exe --help
