5a08ac7c452a14f89e1fbf23d33a720567947445
Spencer Gilbert <spencer.gilbert@datadoghq.com>
2024-09-09 07:55:29 -0400

    feat: Include origin for buildimages.size metric (#652)

--- send-metrics.sh
+++ send-metrics.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Metrics Origin details:
+# https://github.com/DataDog/dd-source/blob/a060ce7a403c2215c44ebfbcc588e42cd9985aeb/domains/metrics/shared/libs/proto/origin/origin.proto#L144
+
 if [ "$#" -ne 3 ]; then
     echo "usage: $0 <image> <size> <branch>"
     exit 1
@@ -13,7 +16,7 @@ set -e
 set +x
 
 API_KEY=$(aws ssm get-parameter --region us-east-1 --name ci.datadog-agent.datadog_api_key_org2 --with-decryption  --query Parameter.Value --out text)
-
+NOW="$(date '+%s')"
 curl -X POST "https://api.datadoghq.com/api/v2/series" \
     -H "Accept: application/json" \
     -H "Content-Type: application/json" \
@@ -21,13 +24,27 @@ curl -X POST "https://api.datadoghq.com/api/v2/series" \
     --silent -S \
     -d @- << EOF
 {
-    "series":
-    [
-        {
-            "metric": "datadog.buildimages.size",
-            "points": [{"timestamp": $(date '+%s'), "value": ${SIZE} }],
-            "tags": ["image:${IMAGE}", "branch:${BRANCH}"]
+  "series": [
+    {
+      "metadata": {
+        "origin": {
+          "origin_product": 17,
+          "origin_sub_product": 0,
+          "origin_product_detail": 0
         }
-    ]
+      },
+      "metric": "datadog.buildimages.size",
+      "points": [
+        {
+          "timestamp": ${NOW},
+          "value": ${SIZE}
+        }
+      ],
+      "tags": [
+        "image:${IMAGE}",
+        "branch:${BRANCH}"
+      ]
+    }
+  ]
 }
 EOF
