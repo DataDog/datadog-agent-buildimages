9c310959548fa8628082ac4be9db85a48519a5a9
Harmon Herring <harmon.herring@datadoghq.com>
2024-10-24 17:06:13 -0400

    test new path

--- windows/helpers/phase3/install_winsign.ps1
+++ windows/helpers/phase3/install_winsign.ps1
@@ -5,7 +5,7 @@ Write-Host -ForegroundColor Green "Installing Windows Codesign Helper $ENV:WINSI
 
 ## need to have more rigorous download at some point, but
 $codesign_base = "windows_code_signer-$($ENV:WINSIGN_VERSION)-py3-none-any.whl"
-$codesign_wheel = "https://s3.amazonaws.com/dd-agent-omnibus/windows-code-signer/$($codesign_base)"
+$codesign_wheel = "https://s3.amazonaws.com/dd-agent-omnibus/windows_code_signer/$($codesign_base)"
 $codesign_wheel_target = "c:\devtools\$($codesign_base)"
 (New-Object System.Net.WebClient).DownloadFile($codesign_wheel, $codesign_wheel_target)
 
