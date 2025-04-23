6a48d282e0478084833aa73f62641d2112047332
Ofek Lev <ofekmeister@gmail.com>
2025-04-22 21:28:22 -0400

    Reorganize local commands

--- .dda/extend/commands/run/agent/version/__init__.py
+++ .dda/extend/commands/run/agent/version/__init__.py
@@ -5,9 +5,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-import click
-
-from dda.cli.base import dynamic_command
+from dda.cli.base import dynamic_command, pass_app
 
 if TYPE_CHECKING:
     from dda.cli.application import Application
@@ -15,14 +13,14 @@ if TYPE_CHECKING:
 
 
 @dynamic_command(short_help="Display the Agent version")
-@click.pass_obj
+@pass_app
 def cmd(app: Application) -> None:
     """
     Display the Agent version.
     """
     import json
 
-    from _utils.constants import PROJECT_ROOT
+    from utils.constants import PROJECT_ROOT
 
     release_file: Path = PROJECT_ROOT / "release.json"
     data = json.loads(release_file.read_text())
