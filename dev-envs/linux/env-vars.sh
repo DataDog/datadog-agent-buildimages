3a78fe4d35de1d99b7e0a4c92a9764cf7d6aff86
Kevin Fairise <kevin.fairise@datadoghq.com>
2025-05-14 11:14:39 +0200

    Make sure the environment variables are available in dev container

--- dev-envs/linux/env-vars.sh
+++ dev-envs/linux/env-vars.sh
@@ -4,3 +4,7 @@ set -euxo pipefail
 
 set-ev DD_SHARED_DIR "${HOME}/.shared"
 set-ev DD_REPOS_DIR "${HOME}/repos"
+# These environment variables are set in docker directives, we want them available when we ssh into the container
+set-ev DD_CC "${DD_CC}"
+set-ev DD_CXX "${DD_CXX}"
+set-ev DD_CMAKE_TOOLCHAIN "${DD_CMAKE_TOOLCHAIN}"
