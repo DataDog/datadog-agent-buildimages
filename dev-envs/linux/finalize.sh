90514a331affd6767fb5d8e983e69a9c6ab17d3c
Ofek Lev <ofekmeister@gmail.com>
2025-06-10 09:28:30 -0400

    Improve cache handling (#866)

---
+++ dev-envs/linux/finalize.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -l
+IFS=$'\n\t'
+set -euxo pipefail
+
+# Remove setup files
+rm -rf /setup
+
+# Remove side effects of APT installations
+apt-get clean && rm -rf /var/lib/apt/lists/*
+
+# Remove cache directories
+cache_dirs=(
+    "${HOME}/.cache/go-build"
+    "/go/pkg/mod"
+    "${HOME}/.cache/pip"
+    "${HOME}/.cargo/registry"
+    "${HOME}/.cargo/git"
+    "/omnibus/vendor/bundle"
+    "/omnibus/cache"
+    "/tmp/omnibus-git-cache"
+)
+
+for cache_dir in "${cache_dirs[@]}"; do
+    rm -rf "$cache_dir"
+done
