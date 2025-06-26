5b7ba8fe107b5b0f3a3ff597667123599306c871
Jack Phillips <jack.phillips@datadoghq.com>
2025-06-26 13:19:30 -0400

    update write-host calls

--- windows/helpers/phase1/install_wdk.ps1
+++ windows/helpers/phase1/install_wdk.ps1
@@ -8,25 +8,25 @@ $wdk = 'https://go.microsoft.com/fwlink/?linkid=2307500' ##  wdk link
 $isinstalled, $iscurrent = get-installupgradestatus -component "wdk" -keyname "downloadfile" -targetvalue $wdk
 if ($isinstalled) {
     if (-not $iscurrent) {
-        write-host -foregroundcolor yellow "not attempting to upgrade wdk"
+        Write-Host -foregroundcolor yellow "not attempting to upgrade wdk"
     }
     return
 }
 
-write-host -foregroundcolor green installing wdk
+Write-Host -foregroundcolor green installing wdk
 $out = "$($psscriptroot)\wdksetup.exe"
 $sha256 = "59c802a7edf1ecca172ec76a8b07702239ae83ee5ff9d1acb6742b2e224e9227"
-write-host -foregroundcolor green downloading $wdk to $out
+Write-Host -foregroundcolor green downloading $wdk to $out
 
 get-remotefile -remotefile $wdk -localfile $out -verifyhash $sha256
 
 get-childitem $out
 # write file size to make sure it worked
-write-host -foregroundcolor green "file size is $((get-item $out).length)"
+Write-Host -foregroundcolor green "file size is $((get-item $out).length)"
 
 
 start-process $out -argumentlist '/features + /quiet' -wait
 
 
 set-installedversionkey -component "wdk" -keyname "downloadfile" -targetvalue $wdk
-write-host -foregroundcolor green done with wdk
+Write-Host -foregroundcolor green done with wdk
