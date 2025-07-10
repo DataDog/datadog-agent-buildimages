504ba0c9f1f6dea9e5e4d4900cd849d6e13c7b71
Nicolas Schweitzer <nicolas.schweitzer@datadoghq.com>
2025-07-10 09:32:05 +0200

    bound variable

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ WORKDIR="."
 if [[ "$DOCKERFILE" == "dev-envs/linux/Dockerfile" ]]; then WORKDIR="dev-envs/linux"; fi
 CACHE_SOURCE="--cache-from type=registry,ref=registry.ddbuild.io/ci/datadog-agent-buildimages/$IMAGE${ECR_TEST_ONLY}:cache"
 # Do not use cache on periodic pipeline where we want to test our dependencies.
-if [[ "$CI_PIPELINE_SOURCE" == "schedule" || -n "$PREVENT_CACHE" ]]; then 
+if [[ "$CI_PIPELINE_SOURCE" == "schedule" || -n "${PREVENT_CACHE:-}" ]]; then 
     CACHE_SOURCE="--no-cache"
 fi
 PUSH=""
@@ -17,7 +17,7 @@ fi
 # Collect build arguments
 GO_BUILD_ARGS=$(cat go.env | sed -e 's/^/--build-arg /' | tr '\n' ' ')
 DDA_BUILD_ARGS=$(cat dda.env | sed -e 's/^/--build-arg /' | tr '\n' ' ')
-if [[ -f "$BUILD_ARGS_FILE" ]]; then
+if [[ -f "${BUILD_ARGS_FILE:-}" ]]; then
     CUSTOM_BUILD_ARGS=$(cat $BUILD_ARGS_FILE | sed -e 's/^/--build-arg /' | tr '\n' ' ')
 fi
 
@@ -26,14 +26,14 @@ docker buildx build \
 --platform $PLATFORM \
 --pull $PUSH \
 --cache-to type=registry,ref=registry.ddbuild.io/ci/datadog-agent-buildimages/$IMAGE${ECR_TEST_ONLY}:cache,mode=max ${CACHE_SOURCE} \
---build-arg BASE_IMAGE=$BASE_IMAGE \
---build-arg BASE_IMAGE_TAG=$BASE_IMAGE_TAG \
---build-arg ARCH=$ARCH \
---build-arg DD_TARGET_ARCH=$DD_TARGET_ARCH \
---build-arg BUILDENV_REGISTRY=$BUILDENV_REGISTRY \
+--build-arg BASE_IMAGE=${BASE_IMAGE:-} \
+--build-arg BASE_IMAGE_TAG=${BASE_IMAGE_TAG:-} \
+--build-arg ARCH=${ARCH:-} \
+--build-arg DD_TARGET_ARCH=${DD_TARGET_ARCH:-} \
+--build-arg BUILDENV_REGISTRY=${BUILDENV_REGISTRY:-} \
 $GO_BUILD_ARGS \
 $DDA_BUILD_ARGS \
-$CUSTOM_BUILD_ARGS \
+${CUSTOM_BUILD_ARGS:-} \
 --tag registry.ddbuild.io/ci/datadog-agent-buildimages/$IMAGE${ECR_TEST_ONLY}:$IMAGE_VERSION \
 --file $DOCKERFILE $WORKDIR \
 --output type=docker,dest=./$IMAGE-$IMAGE_VERSION.tar
