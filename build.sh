2c292017599373f7141053059730038a00789b77
Nicolas Schweitzer <nicolas.schweitzer@datadoghq.com>
2025-07-21 16:13:07 +0200

    Revert "Revert "feat(glibc): Use the same dockerfile (#902)" (#915)"
    This reverts commit d63ded8768a18592c97bf84c16fbf7e54014c0a7.

---
+++ build.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -euo pipefail
+
+# Build and push to internal ECR
+WORKDIR="."
+if [[ "$DOCKERFILE" == "dev-envs/linux/Dockerfile" ]]; then WORKDIR="dev-envs/linux"; fi
+CACHE_SOURCE="--cache-from type=registry,ref=registry.ddbuild.io/ci/datadog-agent-buildimages/$IMAGE${ECR_TEST_ONLY}:cache"
+# Do not use cache on periodic pipeline where we want to test our dependencies.
+if [[ "$CI_PIPELINE_SOURCE" == "schedule" || -n "${PREVENT_CACHE:-}" ]]; then 
+    CACHE_SOURCE="--no-cache"
+fi
+PUSH=""
+if [[ "$CI_PIPELINE_SOURCE" != "schedule" ]]; then 
+    PUSH="--push"
+fi
+
+# Collect build arguments
+GO_BUILD_ARGS=$(sed -e 's/^/--build-arg /' go.env | tr '\n' ' ')
+DDA_BUILD_ARGS=$(sed -e 's/^/--build-arg /' dda.env| tr '\n' ' ')
+if [[ -f "${BUILD_ARGS_FILE:-}" ]]; then
+    CUSTOM_BUILD_ARGS=$(sed -e 's/^/--build-arg /' "${BUILD_ARGS_FILE}" | tr '\n' ' ')
+fi
+
+echo "Run buildx build"
+docker buildx build \
+--platform $PLATFORM \
+--pull $PUSH \
+--cache-to type=registry,ref=registry.ddbuild.io/ci/datadog-agent-buildimages/$IMAGE${ECR_TEST_ONLY}:cache,mode=max ${CACHE_SOURCE} \
+--build-arg BASE_IMAGE=${BASE_IMAGE:-} \
+--build-arg BASE_IMAGE_TAG=${BASE_IMAGE_TAG:-} \
+--build-arg ARCH=${ARCH:-} \
+--build-arg DD_TARGET_ARCH=${DD_TARGET_ARCH:-} \
+--build-arg BUILDENV_REGISTRY=${BUILDENV_REGISTRY:-} \
+$GO_BUILD_ARGS \
+$DDA_BUILD_ARGS \
+${CUSTOM_BUILD_ARGS:-} \
+--tag registry.ddbuild.io/ci/datadog-agent-buildimages/$IMAGE${ECR_TEST_ONLY}:$IMAGE_VERSION \
+--file $DOCKERFILE $WORKDIR \
+--output type=docker,dest=./$IMAGE-$IMAGE_VERSION.tar
