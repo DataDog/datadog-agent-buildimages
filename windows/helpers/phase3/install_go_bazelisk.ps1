3dc3e5f0e4ad93673df2ef6d62b5b39833b9aea8
Régis Desgroppes <regis.desgroppes@datadoghq.com>
2025-08-15 19:00:18 +0200

    tmp

--- windows/helpers/phase3/install_go_bazelisk.ps1
+++ windows/helpers/phase3/install_go_bazelisk.ps1
@@ -5,8 +5,11 @@ $ErrorActionPreference = 'Stop'
 
 go install github.com/bazelbuild/bazelisk@v1.27.0
 
+ls 'C:\go\1.24.6\go\bin'
+($env:PATH -split ';') | ForEach-Object { [PSCustomObject]@{Path=$_} } | Format-Table -AutoSize
+
 # ln -s bazelisk "$(command -v bazelisk | sed 's/bazelisk$/bazel/')"
-Set-Location (Split-Path (Get-Command bazelisk).Source)
+Set-Location (Split-Path (Get-Command bazelisk.exe).Source)
 try {
     New-Item -ItemType SymbolicLink -Path bazel.exe -Target bazelisk.exe
 } finally {
@@ -14,7 +17,7 @@ try {
 }
 
 # bazelisk_home="$(mktemp -d)"
-$bazeliskHome = New-Item -ItemType Directory -Path (Join-Path [System.IO.Path]::GetTempPath() [System.IO.Path]::GetRandomFileName())
+$bazeliskHome = New-Item -ItemType Directory -Path (Join-Path ([System.IO.Path]::GetTempPath()) ([System.IO.Path]::GetRandomFileName()))
 try {
     # BAZELISK_HOME=$bazelisk_home USE_BAZEL_VERSION=7.6.1 bazel --version | grep -Fq 'bazel 7.6.1'
     $version = & {
@@ -22,7 +25,7 @@ try {
         $env:USE_BAZEL_VERSION = '7.6.1'
         bazel --version
     }
-    if ($version -ne 'bazel 7.6.1') {
+    if ($version -ne 'bazel 7.6.x') {
         Write-Error "Unexpected bazel version: $version"
     }
 } finally {
