946fedf2388e27fd0f35a6dd3391f5a77978f630
Régis Desgroppes <regis.desgroppes@datadoghq.com>
2025-08-19 23:30:18 +0200

    Honor local consistency: ~ -> "$HOME"
    This is to comply with
    https://github.com/DataDog/datadog-agent-buildimages/pull/958#discussion_r2285943792,
    as it turns out local code owners prefer using "$HOME" over ~, despite
    the guaranteed tilde expansion in POSIX Shells:
    - https://pubs.opengroup.org/onlinepubs/9699919799/utilities/V3_chap02.html#tag_18_06_01
    - https://www.gnu.org/software/bash/manual/html_node/Tilde-Expansion.html#Tilde-Expansion-1

--- dev-envs/linux/scripts.sh
+++ dev-envs/linux/scripts.sh
@@ -2,11 +2,11 @@
 IFS=$'\n\t'
 set -euxo pipefail
 
-mkdir ~/.scripts
+mkdir "$HOME"/.scripts
 (
     cd "$(dirname "${BASH_SOURCE[0]}")"/scripts
     for f in *.sh; do
-        install -m 755 "$f" ~/.scripts/"${f%.sh}"
+        install -m 755 "$f" "$HOME"/.scripts/"${f%.sh}"
     done
 )
-~/.scripts/path-prepend ~/.scripts
+"$HOME"/.scripts/path-prepend "$HOME"/.scripts
