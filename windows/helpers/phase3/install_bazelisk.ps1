133a887b90d733035765823894cb514280cc069c
Régis Desgroppes <regis.desgroppes@datadoghq.com>
2025-08-19 08:44:32 +0200

    [ABLD-135] Add `bazelisk` to Windows containers (#953)
    * Add `bazelisk` to Windows containers
    
    As the title implies, this is to install the `bazelisk` command line
    tool (standalone binary distribution) meant to bootstrap `bazel`, since
    the version of `bazel` itself will be branch-pinned for reproducible
    builds.
    
    The change does its best to stick to helpers used in sibling
    installation scripts.
    
    * Make `bazel.exe` a symlink to `bazelisk.exe`
    
    As per review comment:
    https://github.com/DataDog/datadog-agent-buildimages/pull/953#discussion_r2283162334

---
+++ windows/helpers/phase3/install_bazelisk.ps1
@@ -0,0 +1,47 @@
+# Install & verify Bazelisk (standalone binary ditribution) as Bazel bootstrapper
+param (
+    [Parameter(Mandatory = $true)][string]$Version,
+    [Parameter(Mandatory = $true)][string]$Sha256
+)
+
+$ErrorActionPreference = 'Stop'
+$PSNativeCommandUseErrorActionPreference = $true
+Set-StrictMode -Version 3.0
+
+$isInstalled, $isCurrent = Get-InstallUpgradeStatus -Component bazelisk -Keyname version -TargetValue $Version
+if ($isInstalled -and $isCurrent) {
+    Write-Host -ForegroundColor Yellow "Skipping bazelisk v$Version reinstallation"
+    return
+}
+
+$targetDir = "c:\devtools\bazelisk"
+if ($isInstalled -and -not $isCurrent) {
+    Remove-Item -Recurse -Force $targetDir -ErrorAction SilentlyContinue
+}
+New-Item -ItemType Directory -Path $targetDir
+Get-RemoteFile -LocalFile "$targetDir\bazelisk.exe" -RemoteFile "https://github.com/bazelbuild/bazelisk/releases/download/v$Version/bazelisk-windows-amd64.exe" -VerifyHash $Sha256
+Push-Location $targetDir
+try {
+    New-Item -ItemType SymbolicLink -Path "bazel.exe" -Target "bazelisk.exe"
+} finally {
+    Pop-Location
+}
+Add-ToPath -NewPath $targetDir -Global -Local
+Write-Host -ForegroundColor Green "Installed bazelisk v$Version"
+
+$bazeliskHome = (New-Item -ItemType Directory -Path (Join-Path ([System.IO.Path]::GetTempPath()) ([System.IO.Path]::GetRandomFileName()))).FullName
+try {
+    $output = & {
+        $env:BAZELISK_HOME = "$bazeliskHome"
+        $env:USE_BAZEL_VERSION = '7.6.1'
+        bazel --version
+    }
+    if ($output -ne 'bazel 7.6.1') {
+        throw "Unexpected bazel version: '$output'"
+    }
+} finally {
+    Remove-Item -Recurse -Verbose "$bazeliskHome"
+}
+
+Set-InstalledVersionKey -Component bazelisk -Keyname version -TargetValue $Version
+Write-Host -ForegroundColor Green "Verified bazelisk v$Version installation"
