9d2a62ddb47900abb778391aba0c36685f55ae37
Celian Raimbault <celian.raimbault@datadoghq.com>
2025-08-22 10:47:50 +0200

    [add-authanywhere] Used shell script

---
+++ setup/authanywhere.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+if [ "$(uname -m)" == "x86_64" ]; then
+    authanywhere_arch="amd64"
+elif [ "$(uname -m)" == "aarch64" ]; then
+    authanywhere_arch="arm64"
+else
+    echo "Unsupported architecture to install authanywhere: $(uname -m)"
+    exit 1
+fi
+
+# Install authanywhere for infra token management
+curl -OL "binaries.ddbuild.io/dd-source/authanywhere/LATEST/authanywhere-linux-$authanywhere_arch" && \
+    mv "authanywhere-linux-$authanywhere_arch" /bin/authanywhere && \
+    chmod +x /bin/authanywhere
