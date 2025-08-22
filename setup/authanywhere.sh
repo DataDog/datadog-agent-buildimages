a5af2cdd7750f6a232824d4bdcbc6f23494ff418
Célian Raimbault <161456554+CelianR@users.noreply.github.com>
2025-08-22 07:22:30 -0400

    Update setup/authanywhere.sh
    Co-authored-by: Régis Desgroppes <regis.desgroppes@datadoghq.com>

--- setup/authanywhere.sh
+++ setup/authanywhere.sh
@@ -12,7 +12,8 @@ fi
 
 # Install authanywhere for infra token management
 curl -OL https://binaries.ddbuild.io/dd-source/authanywhere/v0.0.2/authanywhere-tar.tar.gz
-tar -xf authanywhere-tar.tar.gz
+sha256sum --check --strict <(echo "05d14b25e4607cc9e14867f2a0f38774869ae609eec890168e57de9a1b428e37 *authanywhere-tar.tar.gz")
+tar -xf authanywhere-tar.tar.gz authanywhere-linux-$authanywhere_arch
 mv "authanywhere-linux-$authanywhere_arch" /usr/local/bin/authanywhere
 chmod +x /usr/local/bin/authanywhere
 rm authanywhere-*
