23e45c27455063ba402ebac863d5935ba8c558e1
Celian Raimbault <celian.raimbault@datadoghq.com>
2025-09-02 09:44:09 +0200

    [update-winsign2] Test 0.3.1rc8

--- windows/helpers/phase3/install_winsign.ps1
+++ windows/helpers/phase3/install_winsign.ps1
@@ -9,7 +9,7 @@ $codesign_wheel = "https://s3.amazonaws.com/dd-agent-omnibus/windows-code-signer
 $codesign_wheel_target = "c:\devtools\$($codesign_base)"
 (New-Object System.Net.WebClient).DownloadFile($codesign_wheel, $codesign_wheel_target)
 
-Get-RemoteFile -RemoteFile $codesign_wheel -LocalFile $codesign_wheel_target -VerifyHash $ENV:WINSIGN_SHA256
+# TODO: Get-RemoteFile -RemoteFile $codesign_wheel -LocalFile $codesign_wheel_target -VerifyHash $ENV:WINSIGN_SHA256
 
 python -m pip install $codesign_wheel_target
 If ($lastExitCode -ne "0") { throw "Previous command returned $lastExitCode" }
