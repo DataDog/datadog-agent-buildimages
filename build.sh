9ed875b1ee86027f6bc65a8ea242e14905c05ac0
Pierre-Louis Veyrenc <pierrelouis.veyrenc@datadoghq.com>
2025-09-30 10:54:26 +0200

    [ACIX-1069] Fix docker image caching (#993)
    * fix(build.sh): Include architecture info in cache key
    
    * feat(build.sh): Use two-layered cache approach
    
    We include the branch name in the cache key for our images.
    Then, if we are running on a PR branch, the build can pull the cache from both the branch-related cache (i.e. the build cache from previous commits on the PR), falling back to main if there is no hit.
    When pushing to the cache, we only push to the branch-related cache. Note that this way the `main`-related cache only gets updated once the `main` commit build runs, i.e. when the PR is merged.
    
    * feat(build.sh): Add `set -x` for easier debugging
    
    * fix(build.sh): Sanitize branch names for docker tag format
    
    * [TEMP] Modify Dockerfile to check cache
    
    * Revert "[TEMP] Modify Dockerfile to check cache"
    
    This reverts commit 53523d2d01af9f7dcd856278e3a560568a74fb16.
    
    * fix(build): Prevent trailing slash at the end of sanitized cache keys
    
    `echo` (or maybe the pipe ?) adds a newline character which would get transformed by `tr` into an underscore.
    
    * fix(build.sh): Refer to official docs
    
    * fix(build.sh): Add comment explaining `\n` in `sanitize`

--- build.sh
+++ build.sh
@@ -1,16 +1,42 @@
 #!/bin/bash
-set -euo pipefail
+set -euxo pipefail
 
 # Build and push to internal ECR
 WORKDIR="."
 if [[ "$DOCKERFILE" == "dev-envs/linux/Dockerfile" ]]; then WORKDIR="dev-envs/linux"; fi
-CACHE_SOURCE="--cache-from type=registry,ref=registry.ddbuild.io/ci/datadog-agent-buildimages/$IMAGE${ECR_TEST_ONLY}:cache"
+
+# == Caching logic == #
+function sanitize() {
+    # Docker uses the Go reference format for images: https://pkg.go.dev/github.com/distribution/reference#pkg-overview
+    # A tag name must match the following regex: /[\w][\w.-]{0,127}/
+    # Git branch names can contain disallowed characters, so we need to sanitize them.
+    # `\n` is included here as the pipe adds a newline to the end of the string - not allowing it would mean we get an extra `_` at the end.
+    echo "$1" | tr -C '\na-zA-Z0-9_.-' '_'
+}
+
+# Setup keys
+BRANCH_NAME="${CI_COMMIT_BRANCH:-unknown}"
+CACHE_KEY_BRANCH="$(sanitize "cache-${BRANCH_NAME}-${DD_TARGET_ARCH}")"
+CACHE_KEY_MAIN="$(sanitize "cache-${CI_DEFAULT_BRANCH}-${DD_TARGET_ARCH}")"
+
+CACHE_DETAILS_BRANCH="type=registry,ref=registry.ddbuild.io/ci/datadog-agent-buildimages/$IMAGE:${CACHE_KEY_BRANCH}"
+CACHE_DETAILS_MAIN="type=registry,ref=registry.ddbuild.io/ci/datadog-agent-buildimages/$IMAGE:${CACHE_KEY_MAIN}"
+
+# Cache pull logic: pull from branch preferentially, fallback to main if no hit
 # Do not use cache on periodic pipeline where we want to test our dependencies.
-if [[ "$CI_PIPELINE_SOURCE" == "schedule" || -n "${PREVENT_CACHE:-}" ]]; then 
-    CACHE_SOURCE="--no-cache"
+if [[ "$CI_PIPELINE_SOURCE" == "schedule" || -n "${PREVENT_CACHE:-}" ]]; then
+    CACHE_PULL_ARGS="--no-cache"
+else
+    CACHE_PULL_ARGS="--cache-from ${CACHE_DETAILS_BRANCH} --cache-from ${CACHE_DETAILS_MAIN}"
 fi
+
+# Cache push logic: only push to branch cache
+# Since on main CACHE_DETAILS_BRANCH == CACHE_DETAILS_MAIN, this works as expected.
+CACHE_PUSH_ARGS="--cache-to ${CACHE_DETAILS_BRANCH}"
+
+# == Image push logic == #
 PUSH=""
-if [[ "$CI_PIPELINE_SOURCE" != "schedule" ]]; then 
+if [[ "$CI_PIPELINE_SOURCE" != "schedule" ]]; then
     PUSH="--push"
 fi
 
@@ -25,7 +51,8 @@ echo "Run buildx build"
 docker buildx build \
 --platform $PLATFORM \
 --pull $PUSH \
---cache-to type=registry,ref=registry.ddbuild.io/ci/datadog-agent-buildimages/$IMAGE${ECR_TEST_ONLY}:cache,mode=max ${CACHE_SOURCE} \
+$CACHE_PUSH_ARGS \
+$CACHE_PULL_ARGS \
 --build-arg BASE_IMAGE=${BASE_IMAGE:-} \
 --build-arg BASE_IMAGE_TAG=${BASE_IMAGE_TAG:-} \
 --build-arg ARCH=${ARCH:-} \
