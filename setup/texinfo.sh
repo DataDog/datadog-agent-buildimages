7e82deb56300c899853a00f65cffb85065022eba
Pierre-Louis Veyrenc <pierrelouis.veyrenc@datadoghq.com>
2025-10-03 14:37:05 +0200

    feat(linux): Use `ADD` instructions instead of shell-based calls to `curl` or `wget`
    This is more integrated with the Docker build, and in particular allows the Docker builder to parallelize downloads of everything it needs to include, and start these downloads at the very beginning of the build process.
    This also uses a Docker-native way of verifying checksums, and is in general a better practice - although it results in a couple more layers (will not be important once we switch to a multistage build)

--- setup/texinfo.sh
+++ setup/texinfo.sh
@@ -2,13 +2,6 @@
 
 set -ex
 
-mkdir /tmp/texinfo
-cd /tmp/texinfo
-git clone https://gnu.googlesource.com/texinfo
-
-cd texinfo
-git reset --hard 60d3edc4b74b4e1e5ef55e53de394d3b65506c47
-
 ./autogen.sh
 
 ./configure
@@ -16,5 +9,3 @@ git reset --hard 60d3edc4b74b4e1e5ef55e53de394d3b65506c47
 make -j$(ncproc)
 
 make install
-
-rm -rf /tmp/texinfo
