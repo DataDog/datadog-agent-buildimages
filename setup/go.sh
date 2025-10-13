489a7fc777067a52bde58d6cd7c2d4ae81ff0476
Pierre-Louis Veyrenc <pierrelouis.veyrenc@datadoghq.com>
2025-10-13 17:38:52 +0200

    fix(ci): Revert functional changes to non-`linux` images
    + Restore old setup scripts for legacy images

---
+++ setup/go.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -eo pipefail
+
+case $DD_TARGET_ARCH in
+"x64")
+    GO_SHA256="${GO_SHA256_LINUX_AMD64}"
+    MSGO_SHA256="${MSGO_SHA256_LINUX_AMD64}"
+    GOARCH="amd64"
+    ;;
+"aarch64")
+    GO_SHA256="${GO_SHA256_LINUX_ARM64}"
+    MSGO_SHA256="${MSGO_SHA256_LINUX_ARM64}"
+    GOARCH="arm64"
+    ;;
+"armhf")
+    GO_SHA256="${GO_SHA256_LINUX_ARMV6L}"
+    MSGO_SHA256="${MSGO_SHA256_LINUX_ARMV6L}"
+    GOARCH="armv6l"
+    ;;
+*)
+    echo "Unknown or unsupported architecture ${DD_TARGET_ARCH}"
+    exit -1
+esac
+
+echo "Installing upstream Go"
+curl -sL -o /tmp/golang.tar.gz https://go.dev/dl/go${GO_VERSION}.linux-${GOARCH}.tar.gz
+echo "$GO_SHA256  /tmp/golang.tar.gz" | sha256sum --check
+tar -C /usr/local -xzf /tmp/golang.tar.gz && rm -f /tmp/golang.tar.gz
+
+echo "Installing Microsoft Go"
+curl -SL -o /tmp/golang.tar.gz https://aka.ms/golang/release/latest/go${GO_VERSION}-${MSGO_PATCH}.linux-${GOARCH}.tar.gz
+echo "$MSGO_SHA256  /tmp/golang.tar.gz" | sha256sum --check
+mkdir /usr/local/msgo && tar --strip-components=1 -C /usr/local/msgo/ -xzf /tmp/golang.tar.gz && rm -f /tmp/golang.tar.gz;
+
+cat << EOF >> /root/.bashrc
+if [ "\$DD_GO_TOOLCHAIN" = "msgo" ]; then
+    export PATH="/usr/local/msgo/bin:\$PATH"
+    export GOROOT="/usr/local/msgo"
+else
+    export PATH="/usr/local/go/bin:\$PATH"
+    export GOROOT="/usr/local/go"
+fi
+EOF
---
+++ setup/python.sh
@@ -0,0 +1,139 @@
+#!/bin/bash
+
+set -ex
+
+source "$(dirname "${BASH_SOURCE[0]}")"/../python-packages-versions.txt
+
+function detect_distro(){
+    local KNOWN_DISTRIBUTION="(Debian|Ubuntu|RedHat|CentOS|openSUSE|Amazon|Arista|SUSE)"
+    DISTRIBUTION=$(lsb_release -d 2>/dev/null | grep -Eo $KNOWN_DISTRIBUTION  || grep -Eo $KNOWN_DISTRIBUTION /etc/issue 2>/dev/null || grep -Eo $KNOWN_DISTRIBUTION /etc/Eos-release 2>/dev/null || grep -m1 -Eo $KNOWN_DISTRIBUTION /etc/os-release 2>/dev/null || uname -s)
+}
+
+PY3_VERSION=3.12.6
+DD_CONDA_VERSION=4.9.2-7
+
+case $DD_TARGET_ARCH in
+"x64")
+    DD_CONDA_SHA256="91d5aa5f732b5e02002a371196a2607f839bab166970ea06e6ecc602cb446848"
+    CONDA_URL=https://github.com/conda-forge/miniforge/releases/download/${DD_CONDA_VERSION}/Miniforge3-Linux-x86_64.sh
+    ;;
+"aarch64")
+    DD_CONDA_SHA256="ea7d631e558f687e0574857def38d2c8855776a92b0cf56cf5285bede54715d9"
+    CONDA_URL=https://github.com/conda-forge/miniforge/releases/download/${DD_CONDA_VERSION}/Miniforge3-Linux-aarch64.sh
+    ;;
+"armhf")
+    detect_distro
+    echo "Installing Python from source (armhf)"
+    source /root/.bashrc
+
+    if [ -f /etc/debian_version ] || [ "$DISTRIBUTION" == "Debian" ] || [ "$DISTRIBUTION" == "Ubuntu" ]; then
+        DEBIAN_FRONTEND=noninteractive apt-get update && \
+        DEBIAN_FRONTEND=noninteractive apt-get install -y \
+        zip wget build-essential checkinstall libreadline-gplv2-dev \
+        libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev \
+        libc6-dev libbz2-dev libffi-dev zlib1g-dev
+    elif [ -f /etc/redhat-release ] || [ "$DISTRIBUTION" == "RedHat" ] || [ "$DISTRIBUTION" == "CentOS" ] || [ "$DISTRIBUTION" == "Amazon" ]; then
+        yum install -y gcc openssl-devel bzip2-devel libffi-devel wget make perl-IPC-Cmd
+    fi
+    OPENSSL_VERSION="3.0.13"
+    OPENSSL_SHA256="88525753f79d3bec27d2fa7c66aa0b92b3aa9498dafd93d7cfa4b3780cdae313"
+    PYTHON_SHA256="85a4c1be906d20e5c5a69f2466b00da769c221d6a684acfd3a514dbf5bf10a66"
+
+    wget https://github.com/openssl/openssl/releases/download/openssl-$OPENSSL_VERSION/openssl-$OPENSSL_VERSION.tar.gz
+    echo "$OPENSSL_SHA256 openssl-$OPENSSL_VERSION.tar.gz" | sha256sum --check
+    tar xzf openssl-$OPENSSL_VERSION.tar.gz
+    pushd openssl-$OPENSSL_VERSION
+      ./config --prefix=/opt/openssl --openssldir=/usr/local/ssl -Wl,-Bsymbolic-functions -fPIC shared no-ssl2 no-ssl3 linux-generic32
+      make -j $(nproc)
+      make install
+    popd
+    rm -rf openssl-$OPENSSL_VERSION.tar.gz openssl-$OPENSSL_VERSION
+
+    wget https://www.python.org/ftp/python/$PY3_VERSION/Python-$PY3_VERSION.tgz
+    echo "$PYTHON_SHA256 Python-$PY3_VERSION.tgz" | sha256sum --check
+    tar xzf Python-$PY3_VERSION.tgz
+    pushd /Python-$PY3_VERSION
+        ./configure --with-openssl=/opt/openssl --with-openssl-rpath=auto
+        make -j $(nproc)
+        make install
+    popd
+    rm -rf Python-$PY3_VERSION Python-$PY3_VERSION.tgz
+
+    export OPENSSL_DIR=/opt/openssl
+    # setuptools no longer packaged by default so we need to install it manually.
+    python3 -m pip install setuptools==$DD_SETUPTOOLS_VERSION_PY3
+
+    python3 -m pip install distro==1.4.0 wheel==0.40.0
+    python3 -m pip install --no-build-isolation "cython<3.0.0" PyYAML==5.4.1
+
+    # TODO: remove this once we upgrade toolchains
+    # building ada-url is failing because it requires a newer C++ compiler e.g.:
+    # ada_url/ada.h:21:23: fatal error: string_view: No such file or directory
+    #
+    # building msgspec>=0.20.0 is failing because it requires C11 stdatomic.h e.g.:
+    # src/msgspec/_core.c:7:23: fatal error: stdatomic.h: No such file or directory
+    #
+    # so we download the required packages and install them manually except for ada-url
+    # as it's only used for developer telemetry
+    cat > /tmp/constraints.txt << 'EOF'
+msgspec<0.20.0
+EOF
+    python3 -m pip download -v -d /tmp/dda-install -c /tmp/constraints.txt "git+https://github.com/DataDog/datadog-agent-dev.git@${DDA_VERSION}"
+    cd /tmp/dda-install
+    for dist in *; do
+        if [[ ! "$dist" =~ ^ada_url ]]; then
+            python3 -m pip install --no-deps "$dist"
+        fi
+    done
+    cd ..
+    rm -rf /tmp/dda-install
+    # python3 -m pip install "git+https://github.com/DataDog/datadog-agent-dev.git@${DDA_VERSION}"
+    python3 -m dda self telemetry disable
+    python3 -m dda -v self dep sync -f legacy-build
+    exit 0
+    ;;
+*)
+    echo "Unknown or unsupported architecture ${DD_TARGET_ARCH}"
+    exit -1
+esac
+
+curl -fsL -o miniconda.sh $CONDA_URL
+echo "${DD_CONDA_SHA256}  miniconda.sh" | sha256sum --check
+bash miniconda.sh -b
+rm miniconda.sh
+
+PATH="${CONDA_PATH}/condabin:${PATH}"
+conda init bash
+source /root/.bashrc
+
+# Setup pythons
+conda create -n ddpy3 python python=$PY3_VERSION
+
+# Update pip, setuptools and misc deps
+conda activate ddpy3
+pip install -i https://pypi.python.org/simple pip==${DD_PIP_VERSION_PY3}
+pip install setuptools==${DD_SETUPTOOLS_VERSION_PY3}
+pip install --no-build-isolation "cython<3.0.0" PyYAML==5.4.1
+
+pip install uv==${DD_UV_VERSION}
+dda_repo="/tmp/datadog-agent-dev"
+echo "Cloning dda repository..."
+git clone --depth 1 --branch "${DDA_VERSION}" https://github.com/DataDog/datadog-agent-dev.git "${dda_repo}"
+
+pushd "${dda_repo}"
+echo "Installing dda..."
+uv export --no-editable --no-hashes -o requirements.txt
+pip install -r requirements.txt
+popd
+
+echo "Cleaning up repository..."
+rm -rf "${dda_repo}"
+
+dda self telemetry disable
+dda config set update.mode off
+dda -v self dep sync -f legacy-build
+pip uninstall -y cython # remove cython to prevent further issue with nghttp2
+
+conda clean -a
+
+echo "conda activate ddpy3" >> /root/.bashrc
