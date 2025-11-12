6df7131593e506f2bbc9688a74ad279466b26bde
Celian Raimbault <celian.raimbault@datadoghq.com>
2025-11-12 14:59:26 +0100

    [add-datadog-packages-acix] Removed comment

--- build.sh
+++ build.sh
@@ -47,7 +47,7 @@ if [[ -f "${BUILD_ARGS_FILE:-}" ]]; then
     CUSTOM_BUILD_ARGS=$(sed -e 's/^/--build-arg /' "${BUILD_ARGS_FILE}" | tr '\n' ' ')
 fi
 
-# # Pass the CI_JOB_TOKEN if necessary
+# Pass the CI_JOB_TOKEN if necessary
 CI_JOB_TOKEN_ENV=
 case "$IMAGE" in
     gitlab_agent_deploy|linux)
