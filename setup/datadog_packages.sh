74d5972a2e67bceac702329cb2520d819eaa0512
Celian Raimbault <celian.raimbault@datadoghq.com>
2025-11-13 11:08:38 +0100

    [add-datadog-packages-acix] Custom temporary git config

--- setup/datadog_packages.sh
+++ setup/datadog_packages.sh
@@ -10,10 +10,19 @@ DATADOG_PACKAGES_VERSION=bb430d549b551c0aeb466f3f38470971dabdef2c
 set -euo pipefail
 
 if [ -n "$CI_JOB_TOKEN" ]; then
-    export GIT_CONFIG_COUNT=1
-    export GIT_CONFIG_KEY_0=url."https://gitlab-ci-token:${CI_JOB_TOKEN}@gitlab.ddbuild.io/DataDog/".insteadOf
-    export GIT_CONFIG_VALUE_0="https://github.com/DataDog/"
+    # git config --global url."https://gitlab-ci-token:${CI_JOB_TOKEN}@gitlab.ddbuild.io/DataDog/".insteadOf "https://github.com/DataDog/"
+
+    cat > /tmp/gitconfig <<EOF
+[url "https://gitlab-ci-token:${CI_JOB_TOKEN}@gitlab.ddbuild.io/DataDog/"]
+    insteadOf = https://github.com/DataDog/
+EOF
+
+    export GIT_CONFIG_GLOBAL=/dev/null
+    export GIT_CONFIG_SYSTEM=/dev/null
+    export GIT_CONFIG=/tmp/gitconfig
     go env -w GOPRIVATE="github.com/DataDog/*"
 fi
 export PATH="$PATH:$(go env GOPATH)/bin"
 go install github.com/DataDog/datadog-packages/cmd/datadog-package@$DATADOG_PACKAGES_VERSION
+
+rm -f /tmp/gitconfig
