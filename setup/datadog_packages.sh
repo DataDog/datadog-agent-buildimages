a65f5d3674dbeaa856111b1bbc133067e308e17e
Celian Raimbault <celian.raimbault@datadoghq.com>
2025-11-13 11:00:55 +0100

    [add-datadog-packages-acix] Made config temporary

--- setup/datadog_packages.sh
+++ setup/datadog_packages.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
+# This installs datadog-package binary from the datadog-packages repo
+
+DATADOG_PACKAGES_VERSION=bb430d549b551c0aeb466f3f38470971dabdef2c
+
 # Go env might be set only here
 [ -f /root/.bashrc ] && . /root/.bashrc
 
 set -euo pipefail
 
-DATADOG_PACKAGES_VERSION=bb430d549b551c0aeb466f3f38470971dabdef2c
-
 if [ -n "$CI_JOB_TOKEN" ]; then
-    git config --global url."https://gitlab-ci-token:${CI_JOB_TOKEN}@gitlab.ddbuild.io/DataDog/".insteadOf "https://github.com/DataDog/"
+    export GIT_CONFIG_COUNT=1
+    export GIT_CONFIG_KEY_0=url."https://gitlab-ci-token:${CI_JOB_TOKEN}@gitlab.ddbuild.io/DataDog/".insteadOf
+    export GIT_CONFIG_VALUE_0="https://github.com/DataDog/"
     go env -w GOPRIVATE="github.com/DataDog/*"
 fi
 export PATH="$PATH:$(go env GOPATH)/bin"
