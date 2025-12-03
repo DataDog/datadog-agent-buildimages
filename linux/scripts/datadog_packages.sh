aa9365e73e5c15a9e979eac883f399e8802d6c73
Pierre-Louis Veyrenc <pierrelouis.veyrenc@datadoghq.com>
2025-12-03 16:14:03 +0100

    feat(linux): Create docker bake file regrouping all build args
    feat(linux): Simplify up go, python and bazel install scripts for multiarch
    
    We move a lot of the logic from the shell script for these tools directly into the dockerfile/bakefile:
    - Having the versions and checksums in the bakefile allows us to have a single source of truth
    - Having them in the bakefile means we can more easily pass different values based on arch, instead of relying on shell-based ways of detecting the build architecture
    - Using `ADD` directives instead of manual calls to `curl` is more idiomatic and also allows for better performance (works better with caching + parallel downloads)
    - Add a couple more args and split things up (e.g. dda install script) to improve readability and uniformity of all the stages
    
    refactor(linux): Use all tool-specific ARCH definitions
    
    This makes it clearer which argument is used where, and avoids needing globally used `ARGs` (better for caching)
    
    refactor(linux): Remove unused `build.env` files
    
    fix(linux): Propery pass variables to LABEL instructions

--- linux/scripts/datadog_packages.sh
+++ linux/scripts/datadog_packages.sh
@@ -2,9 +2,6 @@
 
 # This installs datadog-package binary from the datadog-packages repo
 
-DATADOG_PACKAGES_VERSION=bb430d549b551c0aeb466f3f38470971dabdef2c
-
-
 # Go env might be set only here
 [ -f /root/.bashrc ] && . /root/.bashrc
 
