14b1e75ece476490e8c1cd4b1ee479d029fcf6b1
Pierre-Louis Veyrenc <pierrelouis.veyrenc@datadoghq.com>
2025-12-05 11:15:39 +0100

    Restore main-branch script to setup/

---
+++ setup/rvm.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+gpg --import /gpg-keys/*
+rm -rf /gpg-keys
+bash get-rvm.sh stable --version 1.29.12
+echo "d2de0b610ee321489e5c673fe749e13be8fb34c0aa08a74446d87f95a17de730  /usr/local/rvm/bin/rvm" | sha256sum --check
+# Setup rvm in the current shell - rvm.sh has some unbound variables that need to be ignored, so we set +u to ignore them
+set +u
+source /etc/profile.d/rvm.sh
+rvm requirements
+rvm install 2.7 --with-openssl-dir=${CONDA_PATH}
+rvm cleanup all
+gem install bundler --version $BUNDLER_VERSION --no-document
+echo 'source /usr/local/rvm/scripts/rvm' >> /root/.bashrc
