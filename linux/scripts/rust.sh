d21a23e3422d439816d61066d30d006e7a2bdcf8
Guillaume Pagnoux <guillaume.pagnoux@datadoghq.com>
2026-01-23 17:04:12 +0100

    feat(linux): rust: add cargo-deny & dd-rust-license-tool to image

--- linux/scripts/rust.sh
+++ linux/scripts/rust.sh
@@ -4,3 +4,6 @@ set -euxo pipefail
 chmod +x ./rustup-init
 ./rustup-init -y --profile minimal --default-toolchain ${RUST_VERSION}
 echo "${RUSTUP_SHA256}  /root/.cargo/bin/rustc" | sha256sum --check
+
+/root/.cargo/bin/cargo install --git https://github.com/DataDog/rust-license-tool dd-rust-license-tool
+/root/.cargo/bin/cargo install cargo-deny --locked
