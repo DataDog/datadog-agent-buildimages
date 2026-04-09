00cfee2b6f7bce3f9a436585f19d6956813334bf
Cédric Van Rompay <97546950+cedricvanrompay-datadog@users.noreply.github.com>
2026-04-09 11:43:35 +0200

    [SINT-5065] Install windows-code-signer.exe (#1124)
    * make install-all.ps1 throw if a subscript exited with a non-zero status
    
    * install windows-code-signer.exe
    
    * Revert "make install-all.ps1 throw if a subscript exited with a non-zero status"
    
    This reverts commit c309ad6e45f648cc7a3106550052567a79be6f63.
    
    * update sha256 hash
    
    * skip if not building container
    
    * put the image version in a docker ARG
    
    * fix COPY --from

---
+++ windows/helpers/phase3/install_windows_code_signer.ps1
@@ -0,0 +1,42 @@
+# Checks the installed version of windows-code-signer.exe.
+# See https://github.com/DataDog/windows-code-signer.
+# The file was downloaded during the "docker build"
+# with a "COPY --from" instruction in the Dockerfile
+
+param (
+    [Parameter(Mandatory=$true)][string]$Version,
+    [Parameter(Mandatory = $true)][string]$Sha256
+)
+
+$DESTINATION = "C:\devtools\windows-code-signer.exe"
+
+if($Env:DD_DEV_TARGET -ne "Container") {
+    # the binary is downloaded in the Dockerfile,
+    # so if we are not building the container
+    # the binary is not present
+    return
+}
+
+if ( -not (Test-Path $DESTINATION)) {
+    Write-Host -ForegroundColor Red "$DESTINATION not found"
+    throw "$DESTINATION not found"
+}
+
+$actualHash = (Get-FileHash -Algorithm SHA256 $DESTINATION).Hash
+if ($actualHash.ToUpper() -ne $Sha256.ToUpper()) {
+    Write-Host -ForegroundColor Red "Hash mismatch for $DESTINATION"
+    Write-Host -ForegroundColor Red "Expected: $Sha256"
+    Write-Host -ForegroundColor Red "Actual:   $actualHash"
+    Remove-Item $DESTINATION
+    throw "Hash mismatch for $DESTINATION. Expected: $Sha256, Actual: $actualHash"
+}
+
+# Verify version
+$versionOutput = (& $DESTINATION version 2>&1) | Out-String
+if ($versionOutput -notmatch [regex]::Escape($Version)) {
+    Write-Host -ForegroundColor Red "Version mismatch for $DESTINATION"
+    Write-Host -ForegroundColor Red "Expected version: $Version"
+    Write-Host -ForegroundColor Red "Actual output: $versionOutput"
+    Remove-Item $DESTINATION
+    throw "Version mismatch for $DESTINATION. Expected: $Version, Got: $versionOutput"
+}
