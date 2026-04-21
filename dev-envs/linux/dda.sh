dad7c220a17cb9977147ab06a3eb1f56d83ec26f
Kevin Fairise <kevin.fairise@datadoghq.com>
2026-04-21 16:31:26 +0200

    Remove testing release jobs + rename GO_ARCH for dda install

--- dev-envs/linux/dda.sh
+++ dev-envs/linux/dda.sh
@@ -4,11 +4,11 @@ set -euxo pipefail
 
 ARCH=$(dpkg --print-architecture) && \
     if [ "$ARCH" = "amd64" ]; then \
-    GO_ARCH="x86_64"; \
+    DDA_ARCH="x86_64"; \
     elif [ "$ARCH" = "arm64" ]; then \
-    GO_ARCH="aarch64"; \
+    DDA_ARCH="aarch64"; \
     fi && \
-    curl -fsSL --retry 4 "https://github.com/DataDog/datadog-agent-dev/releases/download/${DDA_VERSION}/dda-${GO_ARCH}-unknown-linux-gnu.tar.gz" \
+    curl -fsSL --retry 4 "https://github.com/DataDog/datadog-agent-dev/releases/download/${DDA_VERSION}/dda-${DDA_ARCH}-unknown-linux-gnu.tar.gz" \
     | tar -xzf - -C /usr/local/bin && \
     chmod +x /usr/local/bin/dda
 
