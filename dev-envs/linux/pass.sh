047907fd23ce1d6d1ff7e20f1f262129c8e040c1
Kevin Fairise <kevin.fairise@datadoghq.com>
2026-06-11 15:11:51 +0200

    Update pass init

--- dev-envs/linux/pass.sh
+++ dev-envs/linux/pass.sh
@@ -4,10 +4,3 @@ set -euxo pipefail
 
 apt-get update && apt-get install -y pass
 
-# NOTE: We create a separate gpg dir for pass, and configure pass to always use that gpg
-# homedir. This ensures we don't conflict with a forwarded gpg-agent
-export PASS_GPG_HOME=${XDG_CONFIG_HOME}/.config/password-store/gpg
-mkdir -m 700 -p \$PASS_GPG_HOME
-gpg --homedir \$PASS_GPG_HOME --batch --passphrase '' --quick-generate-key --yes password-store
-PASSWORD_STORE_GPG_OPTS="--homedir \$PASS_GPG_HOME" pass init 'password-store'
-set-ev PASSWORD_STORE_GPG_OPTS "--homedir \"${PASS_GPG_HOME}\""
