5ab248c67f00fd188ca3fa02210c747debd07abc
Pierre-Louis Veyrenc <pierrelouis.veyrenc@datadoghq.com>
2026-07-31 18:10:38 +0200

    fix: specify ddgl version properly

--- dev-envs/linux/ddgl.sh
+++ dev-envs/linux/ddgl.sh
@@ -8,6 +8,8 @@ export UV_PYTHON_INSTALL_DIR="${DD_BUILD_INSTALL_ROOT}/dda/base"
 export UV_TOOL_DIR="${DD_BUILD_INSTALL_ROOT}/dda/venv"
 export UV_TOOL_BIN_DIR="/usr/local/bin"
 
+DDGL_VERSION=v0.2.0
+
 (
     umask 0002
     uv tool install "git+https://github.com/DataDog/ddgl-cli.git@${DDGL_VERSION}"
